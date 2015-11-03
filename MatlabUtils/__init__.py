185514d36b588b512577e11e28806bbf5b7a2c9e
Maciek Swat <maciekswat@gmail.com>
2015-11-03 14:12:55 -0500

    Added new files inclcluding matlab scripts and output files

---
+++ MatlabUtils/__init__.py
@@ -0,0 +1,22 @@
+__author__ = 'm'
+
+import sys
+from os.path import *
+
+try:
+
+    import matlab.engine
+
+except ImportError,e:
+    print 'Seems like support for calling Matlab from Python scripts is not enabled'
+    sys.exit()
+
+print "***************** Starting MATLAB Engine *************************"
+matlab_engine = matlab.engine.start_matlab()
+print "***************** MATLAB Engine WORKING *************************"
+
+
+def add_matlab_search_paths(*path_strings):
+    for path_str in path_strings:
+        matlab_engine.matlab_path_set(abspath(expanduser(path_str)))
+        # matlab_engine.matlab_path_set(abspath(expanduser('~/matlab_extern1')))
