c056d42f6d444c118a95d2fa20a8986210848a50
Maciek Swat <maciekswat@gmail.com>
2015-11-05 14:34:05 -0500

    Introducet MatlabIOSimple where I dont try any trickery with class attribute modification but rely on scipy to layout properly matlab structures. Perhaps this is better solution that writing metaclasses anyway

--- matlab_struct_generate.py
+++ matlab_struct_generate.py
@@ -2,10 +2,74 @@ __author__ = 'm'
 
 import numpy as np
 
-from MatlabIO import MatlabIO, MatlabIOREADER
+from MatlabIO import MatlabIO, MatlabIOREADER,MatlabIOSimple
+
+#
+# class pow(object):
+#
+#     def __init__(self):
+#         # MatlabIO.__init__(self)
+#         self.type = 'fft_slep'
+#         self.freqs = np.logspace(np.log10(1), np.log10(200), 50)
+#         self.logTrans = 1
+#         self.wavenum = np.nan
+#         self.bandwidth = 2
+#         self.wavenum = 0
+#
+#         # the following two parameters are specific fft_slep and are different from
+#         # the parameters below that specifiy post-power binning.
+#         self.winSize = .4  # units in seconds
+#         self.winStep = .05  # units in seconds. use nan if computing power for every sample.
+#
+#         self.timeWin = 100
+#         self.timeStep = 50
+#         self.freqBins = self.freqs
+#         self.zscore = 1
+#
+# class eeg(object):
+#
+#     def __init__(self):
+#         # MatlabIO.__init__(self)
+#
+#
+#         self.durationMS = 4000
+#         self.offsetMS = -1000
+#
+#         self.bufferMS = 1000
+#         self.filtfreq = np.array([58, 62])
+#         self.filttype = 'stop'
+#         self.filtorder = 1
+#         self.sampFreq = 500
+#         self.kurtThr = 4
+#
+#         self.HilbertBands = np.array(
+#                             [
+#                                 [4, 8],
+#                                 [8, 12],
+#                                 [30, 50],
+#                                 [70, 100]
+#                             ]
+#                             )
+#
+#         self.HilbertNames = ['Theta (4-8 Hz)','Alpha (8-12 Hz)','Low Gamma (30-50 Hz)','High Gamma (70-100 Hz)'];
+#
+#
+#
+# class params(object):
+#     def __init__(self):
+#         # MatlabIO.__init__(self)
+#         self.eeg = eeg()
+#         self.pow = pow()
 
 
-class pow(MatlabIO):
+# class params_new(object):
+#     def __init__(self):
+#         # MatlabIO.__init__(self)
+#         self.eeg = eeg()
+#         self.pow = pow()
+
+
+class pow(object):
 
     def __init__(self):
         # MatlabIO.__init__(self)
@@ -26,7 +90,7 @@ class pow(MatlabIO):
         self.freqBins = self.freqs
         self.zscore = 1
 
-class eeg(MatlabIO):
+class eeg(object):
 
     def __init__(self):
         # MatlabIO.__init__(self)
@@ -55,11 +119,17 @@ class eeg(MatlabIO):
 
 
 
-class params(MatlabIO):
+class params(object):
     def __init__(self):
         # MatlabIO.__init__(self)
         self.eeg = eeg()
         self.pow = pow()
+
+
+class Serializer(MatlabIOSimple):
+    params = params()
+
+
 
 # class Params(MatlabIO):
 #     def __init__(self):
@@ -88,9 +158,53 @@ if __name__ == "__main__":
     #
     # print 'dupa'
 
+
+    serializer = Serializer()
+    serializer.serialize('params_serializer.mat')
+
+
+    serializer_check = MatlabIOSimple()
+    serializer_check.deserialize('params_serializer.mat')
+
+    print 'dir()=',dir(serializer_check)
+
+    print serializer_check.params.eeg.durationMS
+
+
+    serializer_check.serialize('params_serializer_check.mat')
+
+
+    sys.exit()
+
+
     group_psl = MatlabIOREADER()
     group_psl.deserialize('GroupPSL.mat')
     print group_psl.GroupPSL[0].Subject
+
+    ps2_events_reader = MatlabIOREADER()
+
+    ps2_events_reader.deserialize('PS2Events.mat')
+
+    ps2_events = ps2_events_reader.PS2Events
+
+    print 'ps2_events=',ps2_events
+
+    print 'number of ps2 events = ', len(ps2_events)
+    for i in xrange(1, len(ps2_events)):
+        ev_curr = ps2_events[i]
+        ev_prev = ps2_events[i-1]
+        print 'i=',i,' event = ', ev_curr, ' event_type = ', ev_curr.type, ' session = ',ev_curr.session
+        if ev_curr.session == ev_prev.session:
+            if ev_curr.type=='STIMULATING' and ev_prev.type=='STIMULATING':
+                print 'isi = ', ev_curr.mstime - ev_prev.mstime
+
+    #     if strcmp(PS2Events(i-1).type,'STIMULATING') & strcmp(PS2Events(i).type,'STIMULATING') & PS2Events(i-1).session==PS2Events(i).session
+    #         PS2Events(i).ISI = PS2Events(i).mstime - PS2Events(i-1).mstime;
+    #     else
+    #         PS2Events(i).ISI = nan;
+    #     end
+    # end
+
 
 
 
