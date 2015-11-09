f4ed9b403a5a710ad41eff9b4ae3f0887b07bc70
Maciek Swat <maciekswat@gmail.com>
2015-11-09 16:56:56 -0500

    Added TexUtils package to facilitate easier generation of tex files

--- RamPipeline/RamTask.py
+++ RamPipeline/RamTask.py
@@ -11,15 +11,27 @@ class RamTask(object):
     def __init__(self):
         pass
 
-    def set_file_resources_to_copy(self,*file_resources):
-
-        self.file_resources_to_copy = file_resources
-
-    def set_file_resources_to_move(self,*file_resources, **kwds):
+    def set_file_resources_to_copy(self, *file_resources, **kwds):
         '''
-        Moves file resources to workspace dir
+        setter method that allows users to specify file resources and subfolder of the workspace dir
+        where the file respirces should be copied
+        :param file_resources: arguments list specifying list of files to copy
+        :param kwds: dst - destination directory for the move operation - the option supported here
+        :return:None
+        '''
+
+        for file_resource in file_resources:
+            try:
+                self.file_resources_to_copy[file_resource] = kwds['dst']
+            except LookupError:
+                self.file_resources_to_copy[file_resource] = ''
+
+    def set_file_resources_to_move(self, *file_resources, **kwds):
+        '''
+        setter method that allows users to specify file resources and subfolder of the workspace dir
+        where the file respirces should be moved
         :param file_resources: arguments list specifying list of files to move
-        :param kwds: option supported here is dst - destination directory for the move operation
+        :param kwds: dst - destination directory for the move operation - the option supported here
         :return:None
         '''
 
@@ -30,6 +42,11 @@ class RamTask(object):
                 self.file_resources_to_move[file_resource] = ''
 
     def make_dir_tree(self, dirname):
+        '''
+        attempts to create directory tree
+        :param dirname: directory name
+        :return:None
+        '''
         import os
         import errno
         try:
@@ -39,37 +56,61 @@ class RamTask(object):
                 raise IOError('Could not make directory: ' + dirname)
 
     def copy_file_resources_to_workspace(self):
+        '''
+        Examines dictionary of files to copy and copies listed files to the appropriate workspace folder or its subfolder
+        :return:None
+        '''
 
         import shutil
         import os
 
-        for file_resource, dst_relative_path  in self.file_resources_to_copy:
+        for file_resource, dst_relative_path in self.file_resources_to_copy:
+            if dst_relative_path != '':
+                self.make_dir_tree(os.path.join(self.pipeline.workspace_dir, dst_relative_path))
 
             file_resource_base_name = os.path.basename(file_resource)
             shutil.copy(file_resource, os.path.join(self.pipeline.workspace_dir, dst_relative_path, file_resource_base_name))
 
     def move_file_resources_to_workspace(self):
+        '''
+        Examines dictionary of files to move and moves listed files to the appropriate workspace folder or its subfolder
+        :return:None
+        '''
 
         import shutil
         import os
-        print 'self.file_resources_to_move=',self.file_resources_to_move
-        for file_resource, dst_relative_path  in self.file_resources_to_move.items():
-            # print 'file_resource=',file_resource
-            if dst_path != '':
-                self.make_dir_tree(os.path.join(self.pipeline.workspace_dir, dst_path))
 
+        for file_resource, dst_relative_path in self.file_resources_to_move.items():
+
+            if dst_relative_path != '':
+                self.make_dir_tree(os.path.join(self.pipeline.workspace_dir, dst_relative_path))
 
             file_resource_base_name = os.path.basename(file_resource)
             shutil.move(file_resource, os.path.join(self.pipeline.workspace_dir, dst_relative_path, file_resource_base_name))
 
 
     def get_pipeline(self):
+        '''
+        Returns pipeline object to which a given task belongs to
+        :return:pipeline object
+        '''
         return self.pipeline
 
     def get_workspace_dir(self):
+        '''
+        Returns full path to the workspace dir
+        :return: full path to the workspace dir
+        '''
         return self.pipeline.workspace_dir
 
     def set_pipeline(self,pipeline):
+        '''
+        Initializes reference pointing to the pipeline object to which current task belongs to.
+        This initialization is done automatically when user adds task to the pipeline
+        :param pipeline: pipeline
+        :return:None
+        '''
+
         self.pipeline = pipeline
     #     self.__name='Task'
     #
@@ -81,5 +122,9 @@ class RamTask(object):
 
 
     def run(self):
+        '''
+        Core function of the task object - needs to be reimplmented in each subslacc deriving from RamTask
+        :return:None
+        '''
         pass
 
