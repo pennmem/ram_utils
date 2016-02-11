dfa2fba2c0a6e5ddae3555af4a13330490c86468
busygin <busygin@gmail.com>
2016-02-11 10:50:20 -0500

    Joint FR1&CatFR1 classifier version

---
+++ tests/ps_report_jointFR/run_ps_report.sh
@@ -0,0 +1,1 @@
+python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/busygin/$2 --mount-point="" --python-path=/home1/busygin/ram_utils_new_ptsa --python-path=/home1/busygin/python/ptsa_new
