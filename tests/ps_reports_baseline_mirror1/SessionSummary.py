03ac82d004fa8389ca491f3c02982084db7c227a
busygin <busygin@gmail.com>
2016-02-12 11:21:27 -0500

    PS code with corrected mirroring

---
+++ tests/ps_reports_baseline_mirror1/SessionSummary.py
@@ -0,0 +1,30 @@
+__author__ = 'm'
+
+import numpy as np
+
+class SessionSummary(object):
+    def __init__(self):
+        self.sess_num = None
+        self.plots = None
+        self.stimtag = None
+        self.region_of_interest = None
+        self.const_param_value = None
+        self.name = None
+        self.date = None
+        self.length = None
+        self.isi_mid = None
+        self.isi_half_range = None
+        self.anova_fvalues = [np.nan, np.nan, np.nan]
+        self.anova_pvalues = [np.nan, np.nan, np.nan]
+        # self.param1_best = None
+        # self.t1 = None
+        # self.p1 = None
+        self.param1_ttest_table = None
+        # self.param2_best = None
+        # self.t2 = None
+        # self.p2 = None
+        self.param2_ttest_table = None
+        # self.param12_best = None
+        # self.t12 = None
+        # self.p12 = None
+        self.param12_ttest_table = None
---
+++ tests/ps_reports_baseline_mirror1/run_ps_report.sh
@@ -0,0 +1,1 @@
+python ps_report.py --subject=$1 --experiment=PS2 --workspace-dir=/scratch/busygin/PS2 --mount-point="" --python-path=/home1/busygin/ram_utils_new_ptsa --python-path=/home1/busygin/python/ptsa_new
