3061c59bb288db2fcf32e21e9ec39bb289fa0d45
busygin <busygin@gmail.com>
2016-02-18 14:12:32 -0500

    Set up paths for losu

--- tests/fr1_report/run_fr1_report.sh
+++ tests/fr1_report/run_fr1_report.sh
@@ -1,1 +1,1 @@
-python fr1_report.py --subject=$1 --task=RAM_FR1 --workspace-dir=/scratch/busygin/FR1_reports --mount-point='' --python-path=/home1/busygin/ram_utils --python-path=/home1/busygin/python/ptsa/build/lib.linux-x86_64-2.7
+python fr1_report.py --subject=$1 --task=RAM_FR1 --workspace-dir=/scratch/pwanda/FR1_reports --mount-point='' --python-path=/home2/losu/ram_utils --python-path=/home2/losu/python/ptsa_latest
