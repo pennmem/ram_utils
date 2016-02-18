6436a02569c4c792b2d21ab601778354505ed96c
busygin <busygin@gmail.com>
2016-02-18 14:07:16 -0500

    Set up losu paths

--- tests/ps_report/run_ps_report.sh
+++ tests/ps_report/run_ps_report.sh
@@ -1,1 +1,1 @@
-python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/busygin/$2 --mount-point="" --python-path=/home1/busygin/ram_utils_new_ptsa --python-path=/home1/busygin/python/ptsa_new
+python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/pwanda/$2 --mount-point="" --python-path=/home2/losu/ram_utils --python-path=/home2/losu/python/ptsa_latest
