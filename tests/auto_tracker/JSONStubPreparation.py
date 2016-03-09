1ee685bec12d518172e10727640d0cf7fdaa3a11
Maciek Swat <maciekswat@gmail.com>
2016-03-09 18:01:42 -0500

    added demo showing how to implement automatic tracking of dependent files

---
+++ tests/auto_tracker/JSONStubPreparation.py
@@ -0,0 +1,47 @@
+__author__ = 'm'
+
+import os
+import os.path
+import numpy as np
+
+from DataMonitor import RamPopulator
+
+from RamPipeline import *
+from JSONUtils import JSONNode
+
+class JSONStubPreparation(RamTask):
+    def __init__(self, params, mark_as_completed=True):
+        RamTask.__init__(self, mark_as_completed)
+        self.params = params
+
+        self.add_dependent_resource(resource_name='FR1_events',
+                                    json_node_access_list = ['experiments','FR1','events'])
+
+    # def run(self):
+    #     json_stub_path = join(self.pipeline.workspace_dir,'_status','index.json')
+    #     if not isfile(json_stub_path):
+    #         rp = RamPopulator()
+    #         j_stub = rp.create_subject_JSON_stub(subject_code=self.pipeline.subject)
+    #         print j_stub.output()
+    #
+    #         j_stub.write(json_stub_path)
+    #     else:
+    #
+    #         # j_stub = JSONNode.read(filename=json_stub_path)
+    #         j_stub = self.pipeline.get_status()
+    #         print 'READ JSON = ',j_stub.output()
+
+    def run(self):
+        print 'RUNNING JSONStubPreparation'
+        # json_stub_path = join(self.pipeline.workspace_dir,'_status','index.json')
+        # if not isfile(json_stub_path):
+        #     rp = RamPopulator()
+        #     j_stub = rp.create_subject_JSON_stub(subject_code=self.pipeline.subject)
+        #     print j_stub.output()
+        #
+        #     j_stub.write(json_stub_path)
+        # else:
+        #
+        #     # j_stub = JSONNode.read(filename=json_stub_path)
+        #     j_stub = self.pipeline.get_status()
+        #     print 'READ JSON = ',j_stub.output()
