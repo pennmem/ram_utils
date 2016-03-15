91ec674e7f7dee93cdf4fd035b065c4ef12d8643
Maciek Swat <maciekswat@gmail.com>
2016-03-15 14:06:34 -0400

    REorganized files - introduces ReportUtils

--- tests/auto_tracker/auto_tracker.py
+++ tests/auto_tracker/auto_tracker.py
@@ -28,9 +28,11 @@ configure_python_paths(args.python_path)
 
 
 import numpy as np
-from RamPipeline import RamPipeline
 
-from RamPipeline.DependencyChangeTrackerLegacy import DependencyChangeTrackerLegacy
+from ReportUtils import ReportPipeline
+# from RamPipeline import RamPipeline
+#
+# from RamPipeline.DependencyChangeTrackerLegacy import DependencyChangeTrackerLegacy
 
 from FREventPreparation import FREventPreparation
 from JSONStubPreparation import JSONStubPreparation
@@ -73,17 +75,17 @@ class Params(object):
 params = Params()
 
 
-class ReportPipeline(RamPipeline):
-    def __init__(self, subject, experiment, workspace_dir, mount_point=None):
-        RamPipeline.__init__(self)
-        self.subject = subject
-        #self.task = 'RAM_FR1'
-        self.experiment = experiment
-        self.mount_point = mount_point
-        self.set_workspace_dir(workspace_dir)
-        dependency_tracker = DependencyChangeTrackerLegacy(subject=subject, workspace_dir=workspace_dir, mount_point=mount_point)
-
-        self.set_dependency_tracker(dependency_tracker=dependency_tracker)
+# class ReportPipeline(RamPipeline):
+#     def __init__(self, subject, experiment, workspace_dir, mount_point=None):
+#         RamPipeline.__init__(self)
+#         self.subject = subject
+#         #self.task = 'RAM_FR1'
+#         self.experiment = experiment
+#         self.mount_point = mount_point
+#         self.set_workspace_dir(workspace_dir)
+#         dependency_tracker = DependencyChangeTrackerLegacy(subject=subject, workspace_dir=workspace_dir, mount_point=mount_point)
+#
+#         self.set_dependency_tracker(dependency_tracker=dependency_tracker)
 
 
 
