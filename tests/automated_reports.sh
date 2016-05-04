e841a147175dc7d6ea2c8deb177e73f2c6338fae
Maciek Swat <maciekswat@gmail.com>
2016-05-04 14:32:51 -0400

    added cleanup functionality - it automatically removes old status dirs and old error logs

--- tests/automated_reports.sh
+++ tests/automated_reports.sh
@@ -17,6 +17,21 @@ export PYTHONPATH="/home1/mswat/extra_libs":$PYTHONPATH
 #source /home1/mswat/.bashrc
 #module load Tex/2014
 
+
+
+function remove_old_status_dirs {
+    # this function removes all status output dirs that begin with yesterdays date
+    # date -v -1d '+%Y_%m_%d'|  xargs bash -c   'ls -d $0*' | xargs rm -rf
+    date -v -1d '+%Y_%m_%d'|  xargs bash -c   'find . -name "$0*" -mindepth 1 -maxdepth 1 -type d' | xargs rm -rf
+}
+
+
+function remove_old_error_logs {
+    date -v -1d '+%Y_%m_%d'|  xargs bash -c   'ls $0*' | xargs rm -rf
+}
+
+
+
 current_directory=$(pwd)
 
 datetime=$(date '+%Y_%m_%d_%H_%M_%S')
@@ -35,6 +50,8 @@ exit_on_no_change_flag=--exit-on-no-change
 # FR1
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr1_report
 
+remove_old_status_dirs
+
 workspace_dir=/scratch/mswat/automated_reports/FR1_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -46,6 +63,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr1_report/fr1_report_all.py  --task=RAM
 
 # FR3
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr_stim_report
+
+remove_old_status_dirs
 
 workspace_dir=${automated_reports_dir}/FR3_reports
 status_output_dir=${workspace_dir}/${datetime}
@@ -59,6 +78,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr_stim_report/fr_stim_report_all.py  --
 # FR4
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr_stim_report
 
+remove_old_status_dirs
+
 workspace_dir=${automated_reports_dir}/FR4_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -71,6 +92,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr_stim_report/fr_stim_report_all.py  --
 #CatFR1
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr1_report
 
+remove_old_status_dirs
+
 workspace_dir=${automated_reports_dir}/CatFR1_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -81,6 +104,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr1_report/fr1_report_all.py  --task=RAM
 
 # FR1 CatFR1 joint
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr1_catfr1_joint_report
+
+remove_old_status_dirs
 
 workspace_dir=${automated_reports_dir}/FR1_CatFr1_reports
 status_output_dir=${workspace_dir}/${datetime}
@@ -94,6 +119,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr1_catfr1_joint_report/fr1_catfr1_joint
 # PAL1
 cd /home1/mswat/RAM_UTILS_GIT/tests/pal1_report
 
+remove_old_status_dirs
+
 workspace_dir=${automated_reports_dir}/PAL1_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -104,6 +131,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/pal1_report/pal1_report_all.py  --task=R
 
 # PS1
 cd /home1/mswat/RAM_UTILS_GIT/tests/ps_report
+
+remove_old_status_dirs
 
 workspace_dir=${automated_reports_dir}/PS1_reports
 status_output_dir=${workspace_dir}/${datetime}
@@ -117,6 +146,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/ps_report/ps_report_all.py  --experiment
 # PS2
 cd /home1/mswat/RAM_UTILS_GIT/tests/ps_report
 
+remove_old_status_dirs
+
 workspace_dir=${automated_reports_dir}/PS2_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -128,6 +159,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/ps_report/ps_report_all.py  --experiment
 # PS2.1
 cd /home1/mswat/RAM_UTILS_GIT/tests/ps_report
 
+remove_old_status_dirs
+
 workspace_dir=${automated_reports_dir}/PS2.1_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -137,6 +170,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/ps_report/ps_report_all.py  --experiment
 
 # PS3
 cd /home1/mswat/RAM_UTILS_GIT/tests/ps_report
+
+remove_old_status_dirs
 
 workspace_dir=${automated_reports_dir}/PS3_reports
 status_output_dir=${workspace_dir}/${datetime}
@@ -148,6 +183,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/ps_report/ps_report_all.py  --experiment
 
 # FR3
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr_stim_report
+
+remove_old_status_dirs
 
 workspace_dir=${automated_reports_dir}/FR3_reports
 status_output_dir=${workspace_dir}/${datetime}
@@ -161,6 +198,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr1_report/fr_stim_report_all.py  --task
 # FR4
 cd /home1/mswat/RAM_UTILS_GIT/tests/fr_stim_report
 
+remove_old_status_dirs
+
 workspace_dir=${automated_reports_dir}/FR4_reports
 status_output_dir=${workspace_dir}/${datetime}
 status_output_dirs+=(${status_output_dir})
@@ -173,6 +212,8 @@ python /home1/mswat/RAM_UTILS_GIT/tests/fr1_report/fr_stim_report_all.py  --task
 
 ## TH1
 cd /home1/mswat/RAM_UTILS_GIT/tests/th1_report
+
+remove_old_status_dirs
 
 workspace_dir=${automated_reports_dir}/TH1_reports
 status_output_dir=${workspace_dir}/${datetime}
@@ -199,6 +240,11 @@ python /home1/mswat/RAM_UTILS_GIT/tests/th1_report/th1_report_all.py  --task=RAM
 
 python /home1/mswat/RAM_UTILS_GIT/ReportUtils/ReportMailer.py\
  --status-output-dirs ${status_output_dirs[@]} --error-log-file=${automated_reports_dir}/error_logs/${datetime}.error.txt
+
+
+#removing old error logs
+cd ${automated_reports_dir}/error_logs
+remove_old_error_logs
 
 
 cd ${current_directory}
