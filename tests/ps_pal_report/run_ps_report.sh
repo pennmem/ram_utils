ed91bf2156c7271b39dbedf5548f299c27cb0b38
busygin <busygin@gmail.com>
2016-05-26 11:36:33 -0400

    shell script typo fix

--- tests/ps_pal_report/run_ps_report.sh
+++ tests/ps_pal_report/run_ps_report.sh
@@ -1,1 +1,1 @@
-python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/pwanda/$2 --mount-point="" --python-path=/home2/losu/ram_utils --python-path=/home2/losu/python/ptsa_latest --python-path=/home1/mswat/extra_libs
+python ps_pal_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/pwanda/$2 --mount-point="" --python-path=/home2/losu/ram_utils --python-path=/home2/losu/python/ptsa_latest --python-path=/home1/mswat/extra_libs
