8427e42aadc634a13d96a936041e27a3ad16a341
busygin <busygin@gmail.com>
2016-05-27 17:41:10 -0400

    Correct amplitude

--- tests/pal3_biomarker/make_pal3_biomarker.sh
+++ tests/pal3_biomarker/make_pal3_biomarker.sh
@@ -9,6 +9,6 @@ cathode=LPT6
 cathode_num=20
 pulse_frequency=100
 pulse_count=50
-target_amplitude=1000
+target_amplitude=1250
 
 python pal3_biomarker.py --workspace-dir=$workspace --subject=$subject --n-channels=$n_channels --anode=$anode --anode-num=$anode_num --cathode=$cathode --cathode-num=$cathode_num --pulse-frequency=$pulse_frequency --pulse-count=$pulse_count --target-amplitude=$target_amplitude
