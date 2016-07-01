1745751abf996bc5e4a2487db73678e87faf155f
akadel <akadel@upenn.edu>
2016-07-01 18:07:02 -0400

    Clarified caption for biomarker contingent analysis

--- tests/fr_stim_report_db/fr_stim_report.py
+++ tests/fr_stim_report_db/fr_stim_report.py
@@ -7,7 +7,7 @@ from ReportUtils import CMLParser,ReportPipeline
 cml_parser = CMLParser(arg_count_threshold=1)
 cml_parser.arg('--subject','R1076D')
 cml_parser.arg('--task','RAM_FR4')
-cml_parser.arg('--workspace-dir','/scratch/busygin/FR4_reports_db')
+cml_parser.arg('--workspace-dir','/scratch/pwanda/FR4_reports_db')
 cml_parser.arg('--mount-point','')
 #cml_parser.arg('--recompute-on-no-status')
 
