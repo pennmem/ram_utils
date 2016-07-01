5efaccde6a57bbf234bb214c81f4c0c0459e1299
busygin <busygin@gmail.com>
2016-07-01 15:18:12 -0400

    TH3 biomarker: first draft

---
+++ tests/th3_biomarker/make_th3_biomarker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+workspace=/scratch/busygin/TH3_biomarkers
+subject=R1191J
+n_channels=256
+anode=RX1
+anode_num=1
+cathode=RX2
+cathode_num=2
+pulse_frequency=200
+pulse_duration=500
+target_amplitude=1000
+
+python th3_biomarker.py --workspace-dir=$workspace --subject=$subject --n-channels=$n_channels --anode=$anode --anode-num=$anode_num --cathode=$cathode --cathode-num=$cathode_num --pulse-frequency=$pulse_frequency --pulse-duration=$pulse_duration --target-amplitude=$target_amplitude
