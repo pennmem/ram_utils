117ca1dd0694766b2617805f0ad57884c0746349
busygin <busygin@gmail.com>
2016-07-12 16:37:30 -0400

    Removed incorrect python-path

--- clinical_affairs/run_ps_report_db.sh
+++ clinical_affairs/run_ps_report_db.sh
@@ -1,3 +1,3 @@
 cd ../tests/ps_report_db
-python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/pwanda/$2 --mount-point="" --python-path=/home2/losu/ram_utils --python-path=/home2/losu/python/ptsa_latest --python-path=/home1/mswat/extra_libs
+python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/pwanda/$2 --mount-point=""
 cd -
