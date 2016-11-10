f4accaccd6f7af3b1e8fa35662ffdda3697f2ce3
busygin <busygin@gmail.com>
2016-11-10 11:59:17 -0500

    connectivity report script for clinical affairs

--- clinical_affairs/run_fr1_catfr1_connectivity_report.sh
+++ clinical_affairs/run_fr1_catfr1_connectivity_report.sh
@@ -1,3 +1,5 @@
-cd ../tests/fr_connectivity_report
-python fr_connectivity_report.py --subject=$1 --workspace-dir=/scratch/pwanda/FR1_CatFR1_connectivity_reports --mount-point=''
-cd -
+#!/bin/bash
+
+source $HOME/.bashrc
+
+qsub $HOME/ram_utils/tests/fr_connectivity_report.sh
