a9a7f23c1f2679f0da75f02dc0619ca4445bb29f
leond <lad258@cornell.edu>
2017-02-06 10:01:59 -0500

    EvaluateClassifier restores correctly

--- tests/fr_stim_report/fr_stim_report.py
+++ tests/fr_stim_report/fr_stim_report.py
@@ -5,9 +5,9 @@ from ReportUtils import CMLParser,ReportPipeline
 
 
 cml_parser = CMLParser(arg_count_threshold=1)
-cml_parser.arg('--subject','R1236J')
-cml_parser.arg('--task','catFR3')
-cml_parser.arg('--workspace-dir','/scratch/leond/catFR3_reports')
+cml_parser.arg('--subject','R1154D')
+cml_parser.arg('--task','FR3')
+cml_parser.arg('--workspace-dir','/scratch/leond/FR3_reports')
 cml_parser.arg('--mount-point','')
 #cml_parser.arg('--recompute-on-no-status')
 
@@ -109,7 +109,7 @@ report_pipeline.add_task(ComputeFRStimPowers(params=params, mark_as_completed=Tr
 
 report_pipeline.add_task(ComputeFRStimTable(params=params, mark_as_completed=False))
 
-report_pipeline.add_task(EvaluateClassifier(params=params,mark_as_completed=False)) #Note: this IS included in the pdf!
+report_pipeline.add_task(EvaluateClassifier(params=params,mark_as_completed=True))
 
 report_pipeline.add_task(ComposeSessionSummary(params=params, mark_as_completed=False))
 #
