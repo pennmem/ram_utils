982dea2585db7a896848a5f1b1ae0203e957df31
leond <lad258@cornell.edu>
2017-02-10 14:02:08 -0500

    restores are now backwards-compatible with existing reports

--- tests/th1_report/th1_report.py
+++ tests/th1_report/th1_report.py
@@ -14,9 +14,9 @@ from ReportUtils import CMLParser,ReportPipeline
 
 
 cml_parser = CMLParser(arg_count_threshold=1)
-cml_parser.arg('--subject','R1201P_1')
+cml_parser.arg('--subject','R1237C')
 cml_parser.arg('--task','TH1')
-cml_parser.arg('--workspace-dir','/scratch/leond/TH_reports')
+cml_parser.arg('--workspace-dir','/scratch/leond/TH1_reports')
 cml_parser.arg('--mount-point','')
 #cml_parser.arg('--recompute-on-no-status')
 #cml_parser.arg('--exit-on-no-change')
