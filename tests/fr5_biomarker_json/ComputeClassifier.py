2dca8bfaea8a898e72b93f194710034eb237705a
Maciek Swat <maciekswat@gmail.com>
2017-02-20 17:09:52 -0500

    added fr5_biomarker_json script

---
+++ tests/fr5_biomarker_json/ComputeClassifier.py
@@ -0,0 +1,268 @@
+from RamPipeline import *
+
+import numpy as np
+from scipy.stats.mstats import zscore
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, roc_curve
+from random import shuffle
+from sklearn.externals import joblib
+from ptsa.data.readers.IndexReader import JsonIndexReader
+
+import hashlib
+
+
+def normalize_sessions(pow_mat, events):
+    sessions = np.unique(events.session)
+    for sess in sessions:
+        sess_event_mask = (events.session == sess)
+        pow_mat[sess_event_mask] = zscore(pow_mat[sess_event_mask], axis=0, ddof=1)
+    return pow_mat
+
+
+class ModelOutput(object):
+    def __init__(self, true_labels, probs):
+        self.true_labels = np.array(true_labels)
+        self.probs = np.array(probs)
+        self.auc = np.nan
+        self.fpr = np.nan
+        self.tpr = np.nan
+        self.thresholds = np.nan
+        self.jstat_thresh = np.nan
+        self.jstat_quantile = np.nan
+        self.low_pc_diff_from_mean = np.nan
+        self.mid_pc_diff_from_mean = np.nan
+        self.high_pc_diff_from_mean = np.nan
+
+    def compute_roc(self):
+        try:
+            self.auc = roc_auc_score(self.true_labels, self.probs)
+        except ValueError:
+            return
+        self.fpr, self.tpr, self.thresholds = roc_curve(self.true_labels, self.probs)
+        # idx = np.argmax(self.tpr-self.fpr)
+        # self.jstat_thresh = self.thresholds[idx]
+        # self.jstat_quantile = np.sum(self.probs <= self.jstat_thresh) / float(self.probs.size)
+        self.jstat_quantile = 0.5
+        self.jstat_thresh = np.median(self.probs)
+
+    def compute_tercile_stats(self):
+        thresh_low = np.percentile(self.probs, 100.0 / 3.0)
+        thresh_high = np.percentile(self.probs, 2.0 * 100.0 / 3.0)
+
+        low_terc_sel = (self.probs <= thresh_low)
+        high_terc_sel = (self.probs >= thresh_high)
+        mid_terc_sel = ~(low_terc_sel | high_terc_sel)
+
+        low_terc_recall_rate = np.sum(self.true_labels[low_terc_sel]) / float(np.sum(low_terc_sel))
+        mid_terc_recall_rate = np.sum(self.true_labels[mid_terc_sel]) / float(np.sum(mid_terc_sel))
+        high_terc_recall_rate = np.sum(self.true_labels[high_terc_sel]) / float(np.sum(high_terc_sel))
+
+        recall_rate = np.sum(self.true_labels) / float(self.true_labels.size)
+
+        self.low_pc_diff_from_mean = 100.0 * (low_terc_recall_rate - recall_rate) / recall_rate
+        self.mid_pc_diff_from_mean = 100.0 * (mid_terc_recall_rate - recall_rate) / recall_rate
+        self.high_pc_diff_from_mean = 100.0 * (high_terc_recall_rate - recall_rate) / recall_rate
+
+
+class ComputeClassifier(RamTask):
+    def __init__(self, params, mark_as_completed=True):
+        RamTask.__init__(self, mark_as_completed)
+        self.params = params
+        self.pow_mat = None
+        self.lr_classifier = None
+        self.xval_output = dict()  # ModelOutput per session; xval_output[-1] is across all sessions
+        self.perm_AUCs = None
+        self.pvalue = None
+
+    def input_hashsum(self):
+        subject = self.pipeline.subject
+        tmp = subject.split('_')
+        subj_code = tmp[0]
+        montage = 0 if len(tmp)==1 else int(tmp[1])
+
+        json_reader = JsonIndexReader(os.path.join(self.pipeline.mount_point, 'protocols/r1.json'))
+
+        hash_md5 = hashlib.md5()
+
+        bp_paths = json_reader.aggregate_values('pairs', subject=subj_code, montage=montage)
+        for fname in bp_paths:
+            with open(fname,'rb') as f: hash_md5.update(f.read())
+
+        fr1_event_files = sorted(list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment='FR1')))
+        for fname in fr1_event_files:
+            with open(fname,'rb') as f: hash_md5.update(f.read())
+
+        catfr1_event_files = sorted(list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment='catFR1')))
+        for fname in catfr1_event_files:
+            with open(fname,'rb') as f: hash_md5.update(f.read())
+
+        fr3_event_files = sorted(list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment='FR3')))
+        for fname in fr3_event_files:
+            with open(fname,'rb') as f: hash_md5.update(f.read())
+
+        catfr3_event_files = sorted(list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment='catFR3')))
+        for fname in catfr3_event_files:
+            with open(fname,'rb') as f: hash_md5.update(f.read())
+
+        return hash_md5.digest()
+
+    def run_loso_xval(self, event_sessions, recalls, permuted=False):
+        probs = np.empty_like(recalls, dtype=np.float)
+
+        sessions = np.unique(event_sessions)
+
+        for sess in sessions:
+            insample_mask = (event_sessions != sess)
+            insample_pow_mat = self.pow_mat[insample_mask]
+            insample_recalls = recalls[insample_mask]
+
+            self.lr_classifier.fit(insample_pow_mat, insample_recalls)
+
+            outsample_mask = ~insample_mask
+            outsample_pow_mat = self.pow_mat[outsample_mask]
+            outsample_recalls = recalls[outsample_mask]
+
+            outsample_probs = self.lr_classifier.predict_proba(outsample_pow_mat)[:, 1]
+            if not permuted:
+                self.xval_output[sess] = ModelOutput(outsample_recalls, outsample_probs)
+                self.xval_output[sess].compute_roc()
+                self.xval_output[sess].compute_tercile_stats()
+            probs[outsample_mask] = outsample_probs
+
+        if not permuted:
+            self.xval_output[-1] = ModelOutput(recalls, probs)
+            self.xval_output[-1].compute_roc()
+            self.xval_output[-1].compute_tercile_stats()
+
+        return probs
+
+    def permuted_loso_AUCs(self, event_sessions, recalls):
+        n_perm = self.params.n_perm
+        permuted_recalls = np.array(recalls)
+        AUCs = np.empty(shape=n_perm, dtype=np.float)
+        for i in xrange(n_perm):
+            for sess in event_sessions:
+                sel = (event_sessions == sess)
+                sess_permuted_recalls = permuted_recalls[sel]
+                shuffle(sess_permuted_recalls)
+                permuted_recalls[sel] = sess_permuted_recalls
+            probs = self.run_loso_xval(event_sessions, permuted_recalls, permuted=True)
+            AUCs[i] = roc_auc_score(recalls, probs)
+            print 'AUC =', AUCs[i]
+        return AUCs
+
+    def run_lolo_xval(self, sess, event_lists, recalls, permuted=False):
+        probs = np.empty_like(recalls, dtype=np.float)
+
+        lists = np.unique(event_lists)
+
+        for lst in lists:
+            insample_mask = (event_lists != lst)
+            insample_pow_mat = self.pow_mat[insample_mask]
+            insample_recalls = recalls[insample_mask]
+
+            self.lr_classifier.fit(insample_pow_mat, insample_recalls)
+
+            outsample_mask = ~insample_mask
+            outsample_pow_mat = self.pow_mat[outsample_mask]
+
+            probs[outsample_mask] = self.lr_classifier.predict_proba(outsample_pow_mat)[:, 1]
+
+        if not permuted:
+            xval_output = ModelOutput(recalls, probs)
+            xval_output.compute_roc()
+            xval_output.compute_tercile_stats()
+            self.xval_output[sess] = self.xval_output[-1] = xval_output
+
+        return probs
+
+    def permuted_lolo_AUCs(self, sess, event_lists, recalls):
+        n_perm = self.params.n_perm
+        permuted_recalls = np.array(recalls)
+        AUCs = np.empty(shape=n_perm, dtype=np.float)
+        for i in xrange(n_perm):
+            for lst in event_lists:
+                sel = (event_lists == lst)
+                list_permuted_recalls = permuted_recalls[sel]
+                shuffle(list_permuted_recalls)
+                permuted_recalls[sel] = list_permuted_recalls
+            probs = self.run_lolo_xval(sess, event_lists, permuted_recalls, permuted=True)
+            AUCs[i] = roc_auc_score(recalls, probs)
+            print 'AUC =', AUCs[i]
+        return AUCs
+
+    def run(self):
+        subject = self.pipeline.subject
+
+
+        events = self.get_passed_object('FR_events')
+        self.pow_mat = normalize_sessions(self.get_passed_object('pow_mat'), events)
+
+        # n1 = np.sum(events.recalled)
+        # n0 = len(events) - n1
+        # w0 = (2.0/n0) / ((1.0/n0)+(1.0/n1))
+        # w1 = (2.0/n1) / ((1.0/n0)+(1.0/n1))
+        self.lr_classifier = LogisticRegression(C=self.params.C, penalty=self.params.penalty_type, class_weight='auto',
+                                                solver='liblinear')
+
+        event_sessions = events.session
+        recalls = events.recalled
+
+        sessions = np.unique(event_sessions)
+        if len(sessions) > 1:
+            print 'Performing permutation test'
+            self.perm_AUCs = self.permuted_loso_AUCs(event_sessions, recalls)
+
+            print 'Performing leave-one-session-out xval'
+            self.run_loso_xval(event_sessions, recalls, permuted=False)
+        else:
+            sess = sessions[0]
+            event_lists = events.list
+
+            print 'Performing in-session permutation test'
+            self.perm_AUCs = self.permuted_lolo_AUCs(sess, event_lists, recalls)
+
+            print 'Performing leave-one-list-out xval'
+            self.run_lolo_xval(sess, event_lists, recalls, permuted=False)
+
+        print 'AUC =', self.xval_output[-1].auc
+
+        self.pvalue = np.sum(self.perm_AUCs >= self.xval_output[-1].auc) / float(self.perm_AUCs.size)
+        print 'Perm test p-value =', self.pvalue
+
+        print 'thresh =', self.xval_output[-1].jstat_thresh, 'quantile =', self.xval_output[-1].jstat_quantile
+
+        # Finally, fitting classifier on all available data
+        self.lr_classifier.fit(self.pow_mat, recalls)
+
+        self.pass_object('lr_classifier', self.lr_classifier)
+        self.pass_object('xval_output', self.xval_output)
+        self.pass_object('perm_AUCs', self.perm_AUCs)
+        self.pass_object('pvalue', self.pvalue)
+
+        classifier_path = self.get_path_to_resource_in_workspace(subject + '-lr_classifier.pkl')
+        joblib.dump(self.lr_classifier, classifier_path)
+        # joblib.dump(self.lr_classifier, self.get_path_to_resource_in_workspace(subject + '-lr_classifier.pkl'))
+        joblib.dump(self.xval_output, self.get_path_to_resource_in_workspace(subject + '-xval_output.pkl'))
+        joblib.dump(self.perm_AUCs, self.get_path_to_resource_in_workspace(subject + '-perm_AUCs.pkl'))
+        joblib.dump(self.pvalue, self.get_path_to_resource_in_workspace(subject + '-pvalue.pkl'))
+
+        self.pass_object('classifier_path', classifier_path)
+
+
+
+    def restore(self):
+        subject = self.pipeline.subject
+
+        classifier_path = self.get_path_to_resource_in_workspace(subject + '-lr_classifier.pkl')
+        self.lr_classifier = joblib.load(classifier_path)
+        # self.lr_classifier = joblib.load(self.get_path_to_resource_in_workspace(subject + '-lr_classifier.pkl'))
+        self.xval_output = joblib.load(self.get_path_to_resource_in_workspace(subject + '-xval_output.pkl'))
+        self.perm_AUCs = joblib.load(self.get_path_to_resource_in_workspace(subject + '-perm_AUCs.pkl'))
+        self.pvalue = joblib.load(self.get_path_to_resource_in_workspace(subject + '-pvalue.pkl'))
+
+        self.pass_object('classifier_path', classifier_path)
+        self.pass_object('lr_classifier', self.lr_classifier)
+        self.pass_object('xval_output', self.xval_output)
+        self.pass_object('perm_AUCs', self.perm_AUCs)
+        self.pass_object('pvalue', self.pvalue)
---
+++ tests/fr5_biomarker_json/make_fr5_biomarker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+workspace=/scratch/busygin/FR3_biomarkers
+subject=R1154D
+n_channels=128
+anode=LTCG29
+anode_num=29
+cathode=LTCG30
+cathode_num=30
+pulse_frequency=200
+pulse_duration=500
+target_amplitude=1000
+
+python fr3_biomarker.py --workspace-dir=$workspace --subject=$subject --n-channels=$n_channels --anode=$anode --anode-num=$anode_num --cathode=$cathode --cathode-num=$cathode_num --pulse-frequency=$pulse_frequency --pulse-duration=$pulse_duration --target-amplitude=$target_amplitude
