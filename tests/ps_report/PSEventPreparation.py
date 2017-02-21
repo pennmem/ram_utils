6ad77bbfa9287d91e44257d88d278c70f521f8a0
leond <lad258@cornell.edu>
2017-02-21 12:46:52 -0500

    you now choose behavioral sessions in PS reports, rather than PS sessions.

--- tests/ps_report/PSEventPreparation.py
+++ tests/ps_report/PSEventPreparation.py
@@ -54,11 +54,7 @@ class PSEventPreparation(ReportRamTask):
 
         json_reader = JsonIndexReader(os.path.join(self.pipeline.mount_point, 'protocols/r1.json'))
 
-        if self.pipeline.sessions is None:
-            event_files = sorted(list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment=task)))
-        else:
-            event_files = [json_reader.get_value('task_events',subject=subj_code,montage=montage,experiment=task,session=s)
-                           for s in self.pipeline.sessions]
+        event_files = sorted(list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment=task)))
         events = None
         for sess_file in event_files:
             e_path = os.path.join(self.pipeline.mount_point, str(sess_file))
