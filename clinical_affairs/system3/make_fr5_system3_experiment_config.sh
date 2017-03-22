d07a6e0769ba3f89433e299e9398bb99db708192
leond <lad258@cornell.edu>
2017-03-22 18:19:00 -0400

    make_fr5_system3_experiment_config more defensive

--- clinical_affairs/system3/make_fr5_system3_experiment_config.sh
+++ clinical_affairs/system3/make_fr5_system3_experiment_config.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# NOTE: BY DEFAULT, FR5 CONFIG USES ANODE1 IF BOTH ANODE1 AND ANODE2 ARE PRESENT,
+# NOTE: BY DEFAULT, FR5 CONFIG USES anode1 IF BOTH anode1 AND anode2 ARE PRESENT,
 # AND SIMILARLY FOR CATHODES.
-# IF YOU DESIRE TO USE ANODE2 AND CATHODE2 FOR FR5 CONFIG,
-# PLEASE SET ANODE1 AND CATHODE1 TO ''.
+# IF YOU DESIRE TO USE anode2 AND cathode2 FOR FR5 CONFIG,
+# PLEASE SET anode1 OR cathode1 TO ''
 # THANK YOU.
 
 subject=R1200T
 experiment=PS4
 anode1=LB6
 cathode1=LB7
-anode2=LB7
-cathode2=
+anode2=
+cathode2=LB8
 target_amplitude=1.0
 config_dir='/scratch/pwanda/FR5_biomarkers/electrode_configs'
 
@@ -23,16 +23,17 @@ max_amplitude=2.0
 
 stim_pair_1=${anode1}-${cathode1}
 stim_pair_2=${anode2}-${cathode2}
-if [ -z $anode1 ];
-then stim_pair_1='';
-elif [ -z $cathode1 ];
+if [ -z $anode1 ] || [ -z $cathode1 ];
 then stim_pair_1='';
 fi
-if [ -z $anode2 ];
-then stim_pair_2='';
-elif [ -z $cathode2 ];
+if [ -z $anode2 ] || [ -z $cathode2 ]
 then stim_pair_2='';
 fi
+
+if ([ -z $stim_pair_1 ] || [ -z $stim_pair_2 ]) && [ $experiment == 'PS4' ];
+then echo "Insufficient number of stim pairs for PS4 config"; exit 0
+fi
+
 
 python ../../system_3_utils/odin_config_tool_generator.py --subject=${subject}\
   --contacts-json-output-dir=${config_dir}\
