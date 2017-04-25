50da8659852aa4894274e1da95c217dd1749f3a7
Maciek Swat <maciekswat@gmail.com>
2017-04-25 16:17:11 -0400

    code formatting

--- tests/fr5_biomarker/FREventPreparation.py
+++ tests/fr5_biomarker/FREventPreparation.py
@@ -14,6 +14,7 @@ import hashlib
 from sklearn.externals import joblib
 from ReportTasks.RamTaskMethods import create_baseline_events
 
+
 class FREventPreparation(RamTask):
     def __init__(self, mark_as_completed=True):
         super(FREventPreparation, self).__init__(mark_as_completed)
@@ -57,21 +58,25 @@ class FREventPreparation(RamTask):
         montage = 0 if len(tmp) == 1 else int(tmp[1])
 
         fr1_events_fname = os.path.abspath(
-            os.path.join(self.pipeline.mount_point, 'scratch','jkragel','events_FR5','RAM_FR1', subj_code + '_events.mat'))
+            os.path.join(self.pipeline.mount_point, 'scratch', 'jkragel', 'events_FR5', 'RAM_FR1',
+                         subj_code + '_events.mat'))
 
         json_reader = JsonIndexReader(os.path.join(self.pipeline.mount_point, 'protocols/r1.json'))
-        
+
         if self.pipeline.args.sessions:
-              event_files = [json_reader.get_value('task_events',subject=subj_code,montage=montage,experiment='FR1',session=s)
-                             for s in sorted(self.pipeline.args.sessions)]
+            event_files = [
+                json_reader.get_value('task_events', subject=subj_code, montage=montage, experiment='FR1', session=s)
+                for s in sorted(self.pipeline.args.sessions)]
         else:
             event_files = sorted(
-               list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment='FR1')))
-        fr1_events = np.concatenate([BaseEventReader(filename=event_path).read() for event_path in event_files]).view(np.recarray)
+                list(json_reader.aggregate_values('task_events', subject=subj_code, montage=montage, experiment='FR1')))
+        fr1_events = np.concatenate([BaseEventReader(filename=event_path).read() for event_path in event_files]).view(
+            np.recarray)
 
-        catfr1_events = np.concatenate( [BaseEventReader(filename=event_path).read() for event_path in
-                                         json_reader.aggregate_values('task_events',subject=subj_code,experiment='catFR1',
-                                                                      montage = montage)]).view(np.recarray)
+        catfr1_events = np.concatenate([BaseEventReader(filename=event_path).read() for event_path in
+                                        json_reader.aggregate_values('task_events', subject=subj_code,
+                                                                     experiment='catFR1',
+                                                                     montage=montage)]).view(np.recarray)
 
         # making sure that events have the same columens and do not include stim_params column (which is a composite type
         # that is not used here at all)
@@ -83,14 +88,13 @@ class FREventPreparation(RamTask):
         # col_name_list = list(fr1_events.dtype.names)
         # col_name_list = col_name_list.remove('stim_params')
 
-        catfr1_events = catfr1_events[common_column_list ]
-        fr1_events = fr1_events[common_column_list ]
+        catfr1_events = catfr1_events[common_column_list]
+        fr1_events = fr1_events[common_column_list]
 
         # catfr1_events=catfr1_events[list(fr1_events.dtype.names)]
         catfr1_events.session += 100
-        fr1_events = np.append(fr1_events,catfr1_events).view(np.recarray)
+        fr1_events = np.append(fr1_events, catfr1_events).view(np.recarray)
         fr1_events = create_baseline_events(fr1_events)
-
 
         # e_reader = BaseEventReader(filename=fr1_events_fname, eliminate_events_with_no_eeg=True,common_root='scratch')
         # fr1_events = e_reader.read()
@@ -98,9 +102,10 @@ class FREventPreparation(RamTask):
 
         encoding_events_mask = fr1_events.type == 'WORD'
         retrieval_events_mask = (fr1_events.type == 'REC_WORD') | (fr1_events.type == 'REC_BASE')
-        irts = np.append([0],np.diff(fr1_events.mstime))
+        irts = np.append([0], np.diff(fr1_events.mstime))
         retrieval_events_mask_0s = retrieval_events_mask & (fr1_events.type == 'REC_BASE')
-        retrieval_events_mask_1s = retrieval_events_mask & (fr1_events.type == 'REC_WORD') & (fr1_events.intrusion == 0)  & (irts > 1000)
+        retrieval_events_mask_1s = retrieval_events_mask & (fr1_events.type == 'REC_WORD') & (
+        fr1_events.intrusion == 0) & (irts > 1000)
 
         filtered_events = fr1_events[encoding_events_mask | retrieval_events_mask_0s | retrieval_events_mask_1s]
 
@@ -108,18 +113,13 @@ class FREventPreparation(RamTask):
 
         print len(events), 'WORD events'
 
-        joblib.dump(events,self.get_path_to_resource_in_workspace(subject+'-FR_events.pkl'))
+        joblib.dump(events, self.get_path_to_resource_in_workspace(subject + '-FR_events.pkl'))
         self.pass_object('FR_events', events)
         # self.pass_object('encoding_events_mask',encoding_events_mask)
         # self.pass_object('retrieval_events_mask_0s',retrieval_events_mask_0s)
         # self.pass_object('retrieval_events_mask_1s',retrieval_events_mask_1s)
 
     def restore(self):
-        subject=self.pipeline.subject
-        events = joblib.load(self.get_path_to_resource_in_workspace(subject+'-FR_events.pkl'))
-        self.pass_object('FR_events',events)
-
-
-
-
-
+        subject = self.pipeline.subject
+        events = joblib.load(self.get_path_to_resource_in_workspace(subject + '-FR_events.pkl'))
+        self.pass_object('FR_events', events)
