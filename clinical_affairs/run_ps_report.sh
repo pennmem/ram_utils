7693b285d7d0bf1631d73df9522dc318c7960a25
leond <lad258@cornell.edu>
2017-05-01 13:37:22 -0400

    run_ps_report now handles ps4

--- clinical_affairs/run_ps_report.sh
+++ clinical_affairs/run_ps_report.sh
@@ -1,3 +1,13 @@
+ps_experiment=$2
+if [ $ps_experiment = 'PS4' ]
+then
+read -p "MEMORY TASK [FR5,PAL5]:" memory_task;
+read -p "WORKSPACE DIR:" output_dir
+cd ../tests/ps4_report
+python ps4_report.py --subject=$1 --task=${memory_task} --workspace-dir="$(realpath ${output_dir})" "${@:3}"
+cd-
+else
 cd ../tests/ps_report
 python ps_report.py --subject=$1 --experiment=$2 --workspace-dir=/scratch/pwanda/FR_reports --mount-point="" "${@:3}"
 cd -
+fi
