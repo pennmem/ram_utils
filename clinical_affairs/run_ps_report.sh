e565b2844ca967369b80f5e01d7ab274c5514bfb
leond <lad258@cornell.edu>
2017-05-01 13:57:00 -0400

    run_ps_report now handles ps4; requires valid memory task to proceed

--- clinical_affairs/run_ps_report.sh
+++ clinical_affairs/run_ps_report.sh
@@ -1,7 +1,10 @@
 ps_experiment=$2
-if [ $ps_experiment = 'PS4' ]
+if [ ${ps_experiment} == 'PS4' ];
 then
-read -p "MEMORY TASK [FR5,PAL5]:" memory_task;
+memory_task='NONE'
+while [ $memory_task != 'FR5' ] && [ $memory_task != 'PAL5' ]; do
+read -p "MEMORY TASK [FR5,PAL5]:" memory_task
+done
 read -p "WORKSPACE DIR:" output_dir
 cd ../tests/ps4_report
 python ps4_report.py --subject=$1 --task=${memory_task} --workspace-dir="$(realpath ${output_dir})" "${@:3}"
