44b7450dc3a2c9e2557a959322034c1809265828
leond <lad258@cornell.edu>
2017-05-12 15:42:53 -0400

    clinical affairs reporting scripts now use /scratch/RAM_reports as workspace root directory

--- clinical_affairs/run_th1_report.sh
+++ clinical_affairs/run_th1_report.sh
@@ -1,3 +1,3 @@
 cd ../tests/th1_report
-python th1_report.py --subject=$1 --task=TH1 --workspace-dir=/scratch/pwanda/TH_reports --mount-point='' "${@:2}"
+python th1_report.py --subject=$1 --task=TH1 --workspace-dir=/scratch/RAM_reports/TH1_reports --mount-point='' "${@:2}"
 cd -
