9b2455549bcf284e9d6d4a5f8363c1453468806b
leond <lad258@cornell.edu>
2017-06-19 11:04:10 -0400

    added deploy_report back in

--- tests/ps4_report/ps4_report_all.py
+++ tests/ps4_report/ps4_report_all.py
@@ -4,7 +4,7 @@ from os.path import join
 from ptsa.data.readers.IndexReader import JsonIndexReader
 
 parser=CMLParser()
-parser.arg('--task','catFR5')
+parser.arg('--task','FR5')
 parser.arg('--workspace-dir','ps4_reports')
 parser.arg('--mount-point','/Volumes/rhino_root/')
 parser.arg('--recompute-on-no-status')
@@ -28,7 +28,7 @@ from ReportUtils import ReportSummaryInventory
 rsi = ReportSummaryInventory(label='PS4_%s'%args.task)
 
 jr = JsonIndexReader(join(args.mount_point,'protocols','r1.json'))
-subjects = [s for s in jr.subjects() if jr.aggregate_values('ps4_events',subject=s,experiment=args.task)]
+subjects = [s for s in jr.subjects() if any(jr.aggregate_values('ps4_events',subject=s,experiment=args.task))]
 for subject in subjects:
 
         report_pipeline = ReportPipeline(subject=subject,task=args.task,workspace_dir= join(args.workspace_dir,subject),
@@ -45,7 +45,7 @@ for subject in subjects:
 
         report_pipeline.add_task(GeneratePDF(mark_as_completed=False))
 
-        # report_pipeline.add_task(DeployReportPDF())
+        report_pipeline.add_task(DeployReportPDF())
 
         report_pipeline.execute_pipeline()
 
