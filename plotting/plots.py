e1144ef4b14ffcb4cc878c890a1ffffc64647126
Zach Duey <zachduey@gmail.com>
2017-10-25 16:58:31 -0400

    Starter files for moving plotting away from frakenstein wrapper

---
+++ plotting/plots.py
@@ -0,0 +1,29 @@
+""" Set of utility functions for generating matplotlib plots for RAM reports """
+import os
+import matplotlib.pyplot as plt
+
+
+def roc_curve():
+    return
+
+def tercile_classifier_estimate():
+    return
+
+def probability_of_recall():
+    return
+
+def stim_recall():
+    return
+
+def probability_of_stimulation():
+    return
+
+def delta_recall():
+    return
+
+
+def biomarker_distribution():
+    return
+
+def post_stim_eeg():
+    return
