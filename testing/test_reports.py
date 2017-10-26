7096bf6a99f2da3dcb3ae851027b74ce90089ef2
Zach Duey <zachduey@gmail.com>
2017-10-26 13:11:37 -0400

    Generalized tests to avoid hard coding

--- testing/test_reports.py
+++ testing/test_reports.py
@@ -1,14 +1,24 @@
 """ Functional tests for various reports """
 import os
+import shutil
 import pytest
 import subprocess
 
 
-# Update these before running
-CODE_DIR = "/home1/zduey/ram_utils/"
+# Assumes that testing folder is in the top level of ramutils
+CODE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MOUNT = "/"
-WORKSPACE_BASE = "/scratch/zduey/"
+TEST_DIR = "/scratch/RAM_maint/automated_reports_json/tmp_testing_data/"
 
+def setup_module():
+    """ Called by pytest before any test functions are called """
+    sample_reports = ["samplefr1_reports", "samplefr5_reports", "samplefr5_biomarkers"]
+    for sample_report in sample_reports:
+        tmpdir = TEST_DIR + sample_report
+        if os.path.exists(tmpdir):
+            shutil.rmtree(TEST_DIR + sample_report) # remove and replace
+        os.mkdir(TEST_DIR + sample_report)
+    return
 
 @pytest.mark.parametrize("subject",[
     ("R1308T"),
@@ -18,10 +28,9 @@ WORKSPACE_BASE = "/scratch/zduey/"
 def test_fr1_report(subject):
     os.chdir(CODE_DIR + "/tests/fr1_report/")
     print(os.getcwd())
-    workspace = WORKSPACE_BASE + "samplefr1_reports/"
+    workspace = TEST_DIR + "samplefr1_reports/"
     command = "python fr1_report.py --subject={} --task=FR1 --workspace-dir={} --mount-point={}".format(subject, workspace, MOUNT)
-    retcode = subprocess.call(command, shell=True)
-    assert (retcode == 0)
+    subprocess.check_output(command, shell=True)
     return
 
 @pytest.mark.parametrize("subject",[
@@ -31,10 +40,9 @@ def test_fr1_report(subject):
 )
 def test_fr5_report(subject):
     os.chdir(CODE_DIR + "/tests/fr5_report/")
-    workspace = WORKSPACE_BASE + "samplefr5_reports/"
+    workspace = TEST_DIR + "samplefr5_reports/"
     command = "python fr5_report.py --subject={} --task=FR5 --workspace-dir={} --mount-point={}".format(subject, workspace, MOUNT)
-    retcode = subprocess.call(command, shell=True)
-    assert (retcode == 0)
+    subprocess.check_output(command, shell=True)
     return
 
 @pytest.mark.parametrize("subject, n_channels, anode, cathode, pulse_frequency, pulse_duration, target_amplitude, anode_num, cathode_num",[
@@ -43,7 +51,7 @@ def test_fr5_report(subject):
 )
 def test_fr5_biomarker(subject, n_channels, anode, cathode, pulse_frequency, pulse_duration, target_amplitude, anode_num, cathode_num):
     os.chdir(CODE_DIR + "/tests/fr5_biomarker/")
-    workspace = WORKSPACE_BASE + "samplefr5_biomarkers/"
+    workspace = TEST_DIR + "samplefr5_biomarkers/"
     command = "python fr5_biomarker.py\
               --subject={}\
               --n-channels={}\
@@ -66,7 +74,6 @@ def test_fr5_biomarker(subject, n_channels, anode, cathode, pulse_frequency, pul
                                        cathode_num,
                                        workspace,
                                        MOUNT)
-    retcode = subprocess.call(command, shell=True)
-    assert (retcode == 0)
+    subprocess.check_output(command, shell=True)
     return
 
