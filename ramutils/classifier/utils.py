ee2d104e499a4a8c3c32776948bff5d13d29ea69
Zach Duey <zachduey@gmail.com>
2017-10-27 13:41:59 -0400

    add get_sample_weights function for pal task to classifier utils

--- ramutils/classifier/utils.py
+++ ramutils/classifier/utils.py
@@ -70,7 +70,67 @@ def get_sample_weights(events, encoding_multiplier):
     return weights
 
 
-def get_pal_sample_weights():
-    return
+def get_pal_sample_weights(events, pal_sample_weights, encoding_sample_weights):
+    """ Calculate sample weights based on PAL-specific scheme
 
+    Parameters:
+    -----------
+    events: pd.DataFrame
+    pal_sample_weights: (int) Scalar to determine how much more to weight pal samples
+    encoding_sample_weights: (int) Scalar to determine how much more to weight encoding samples
 
+    Returns
+    -------
+    weights: np.ndarray
+
+    """
+
+    enc_mask = (events.type == 'WORD')
+    retrieval_mask = (events.type == 'REC_EVENT')
+
+    pal_mask = (events.exp_name == 'PAL1')
+    fr_mask = ~pal_mask
+
+    pal_n_enc_0 = events[pal_mask & enc_mask & (events.correct == 0)].shape[0]
+    pal_n_enc_1 = events[pal_mask & enc_mask & (events.correct == 1)].shape[0]
+
+    pal_n_ret_0 = events[pal_mask & retrieval_mask & (events.correct == 0)].shape[0]
+    pal_n_ret_1 = events[pal_mask & retrieval_mask & (events.correct == 1)].shape[0]
+
+    fr_n_enc_0 = events[fr_mask & enc_mask & (events.correct == 0)].shape[0]
+    fr_n_enc_1 = events[fr_mask & enc_mask & (events.correct == 1)].shape[0]
+
+    fr_n_ret_0 = events[fr_mask & retrieval_mask & (events.correct == 0)].shape[0]
+    fr_n_ret_1 = events[fr_mask & retrieval_mask & (events.correct == 1)].shape[0]
+
+    ev_count_list = [pal_n_enc_0, pal_n_enc_1, pal_n_ret_0, pal_n_ret_1, 
+                     fr_n_enc_0, fr_n_enc_1, fr_n_ret_0, fr_n_ret_1]
+
+    n_vec = np.array([0.0] * 8, dtype=np.float)
+
+    for i, ev_count in enumerate(ev_count_list):
+        n_vec[i] = 1. / ev_count if ev_count else 0.0
+
+    n_vec /= np.mean(n_vec)
+
+    # scaling PAL1 task
+    n_vec[0:4] *= pal_sample_weights
+    n_vec /= np.mean(n_vec)
+
+    # scaling encoding
+    n_vec[[0, 1, 4, 5]] *= encoding_sample_weights
+    n_vec /= np.mean(n_vec)
+
+    weights = np.ones(events.shape[0], dtype=np.float)
+
+    weights[pal_mask & enc_mask & (events.correct == 0)] = n_vec[0]
+    weights[pal_mask & enc_mask & (events.correct == 1)] = n_vec[1]
+    weights[pal_mask & retrieval_mask & (events.correct == 0)] = n_vec[2]
+    weights[pal_mask & retrieval_mask & (events.correct == 1)] = n_vec[3]
+
+    weights[fr_mask & enc_mask & (events.correct == 0)] = n_vec[4]
+    weights[fr_mask & enc_mask & (events.correct == 1)] = n_vec[5]
+    weights[fr_mask & retrieval_mask & (events.correct == 0)] = n_vec[6]
+    weights[fr_mask & retrieval_mask & (events.correct == 1)] = n_vec[7]
+    
+    return weights
