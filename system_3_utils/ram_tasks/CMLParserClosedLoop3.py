34a5915c3685c5e66ddba665401619e4148bd3d8
Michael V. DePalatis <mike@depalatis.net>
2017-10-27 10:38:22 -0400

    Cleanup

--- system_3_utils/ram_tasks/CMLParserClosedLoop3.py
+++ system_3_utils/ram_tasks/CMLParserClosedLoop3.py
@@ -1,45 +1,35 @@
-__author__ = 'm'
-
-
 import sys
 import argparse
-import os
 from os.path import *
 
 
+# FIXME: just use ArgumentParser directly; this isn't doing anything that it can't
 class CMLParserCloseLoop3(object):
     def __init__(self, arg_count_threshold=1):
         self.parser = argparse.ArgumentParser(description='Report Generator')
-        self.parser.add_argument('--subject', required=True, action='store')
-        self.parser.add_argument('--experiment', required=False, action='store')
-        self.parser.add_argument('--workspace-dir', required=False, action='store')
-        self.parser.add_argument('--mount-point', required=False, action='store')
-        self.parser.add_argument('--electrode-config-file', required=True, action='store')
-        self.parser.add_argument('--pulse-frequency', required=True, action='store',type=int)
-        self.parser.add_argument('--target-amplitude', required=True, action='store',type=float)
-        # self.parser.add_argument('--stim-electrode-pair', required=True, action='store')
+        self.parser.add_argument('--subject', required=True)
+        self.parser.add_argument('--experiment', required=False)
+        self.parser.add_argument('--workspace-dir', required=False)
+        self.parser.add_argument('--mount-point', required=False)
+        self.parser.add_argument('--electrode-config-file', required=True)
+        self.parser.add_argument('--pulse-frequency', required=True, type=int)
+        self.parser.add_argument('--target-amplitude', required=True, type=float)
 
-        # self.parser.add_argument('--python-path', required=False, action='append')
-        # self.parser.add_argument('--n-channels', required=True, action='store',type=int)
-        self.parser.add_argument('--anode-num',   required= False,action='store',type=int)
-        self.parser.add_argument('--anode',       required= False,action='store',default='')
-        self.parser.add_argument('--cathode-num', required= False,action='store',type=int)
-        self.parser.add_argument('--cathode',     required= False,action='store',default='')
+        self.parser.add_argument('--anode-num', required=False, type=int)
+        self.parser.add_argument('--anode', required=False, default='')
+        self.parser.add_argument('--cathode-num', required=False, type=int)
+        self.parser.add_argument('--cathode', required=False, default='')
 
-        # self.parser.add_argument('--pulse-duration', required=True, action='store',type=int)
-        self.parser.add_argument('--anode-nums',nargs='+',type=int,metavar='ANODE_NUM')
-        self.parser.add_argument('--anodes',nargs='+', metavar='ANODE')
-        self.parser.add_argument('--cathode-nums',nargs='+',type=int, metavar='CATHODE_NUM')
-        self.parser.add_argument('--cathodes',nargs='+',metavar='CATHODE')
-        self.parser.add_argument('--min-amplitudes',nargs='+',type=float)
-        self.parser.add_argument('--max-amplitudes',nargs='+',type=float)
-        self.parser.add_argument('--sessions',nargs='+',type=int)
-        self.parser.add_argument('--encoding-only',action='store_true')
-        # self.parser.add_argument('--bipolar',action="store_true", default=False,
-        #                 help="Enables bipolar referencing - will copy bipolar_2_monopolar_transformation matrix to the config_files ")
-        #
+        self.parser.add_argument('--anode-nums', nargs='+', type=int, metavar='ANODE_NUM')
+        self.parser.add_argument('--anodes', nargs='+', metavar='ANODE')
+        self.parser.add_argument('--cathode-nums', nargs='+', type=int, metavar='CATHODE_NUM')
+        self.parser.add_argument('--cathodes', nargs='+', metavar='CATHODE')
+        self.parser.add_argument('--min-amplitudes', nargs='+', type=float)
+        self.parser.add_argument('--max-amplitudes', nargs='+', type=float)
+        self.parser.add_argument('--sessions', nargs='+', type=int)
+        self.parser.add_argument('--encoding-only', action='store_true')
 
-        self.arg_list=[]
+        self.arg_list = []
         self.arg_count_threshold = arg_count_threshold
 
     def arg(self, name, *vals):
@@ -47,13 +37,13 @@ class CMLParserCloseLoop3(object):
         for val in vals:
             self.arg_list.append(val)
 
-    def configure_python_paths(self,paths):
+    def configure_python_paths(self, paths):
         for path in paths:
             sys.path.append(path)
 
     def parse(self):
         print sys.argv
-        if len(sys.argv)<=self.arg_count_threshold and len(self.arg_list):
+        if len(sys.argv) <= self.arg_count_threshold and len(self.arg_list):
             args = self.parser.parse_args(self.arg_list)
         else:
             args = self.parser.parse_args()
@@ -68,22 +58,10 @@ class CMLParserCloseLoop3(object):
         else:
             args.mount_point = abspath(expanduser(args.mount_point))
 
-        # Converting python search paths to proper format
-        # if not args.python_path:
-        #     args.python_path = [os.getcwd()]
-        # else:
-        #     args.python_path = [abspath(expanduser(path)) for path in args.python_path]
-        #     args.python_path.insert(0, os.getcwd())
-
-
-        # self.configure_python_paths(args.python_path)
-
-        #check that target amplitude is in milliamps
+        # check that target amplitude is in milliamps
         try:
-            assert args.target_amplitude<2 and args.target_amplitude>0
+            assert args.target_amplitude < 2 and args.target_amplitude > 0
         except AssertionError:
             raise ValueError('Target amplitude should be between 0 and 2 milliamps')
 
         return args
-
-
