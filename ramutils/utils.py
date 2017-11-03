7fe53ba37902c40dca200407a45cd069a6d1c71c
Michael V. DePalatis <mike@depalatis.net>
2017-11-03 15:47:22 -0400

    Rework Ramulator config templates

--- ramutils/utils.py
+++ ramutils/utils.py
@@ -1,8 +1,38 @@
-""" Some utility functions for calculations that are done during the course of the reporting pipeline """
+from collections import OrderedDict
+import json
+
+
+def reindent_json(json_file, indent=2):
+    """Re-indent JSON data preserving order. Useful for removing extraneous
+    whitespace leftover from templates generation.
+
+    Parameters
+    ----------
+    json_file : str or file-like
+        Path to JSON file or a file-like object containing JSON data.
+    indent : int
+        Indent width
+
+    Returns
+    -------
+    reindented : str
+        Re-indented JSON string
+
+    """
+    # Path to JSON file
+    if isinstance(json_file, str):
+        with open(json_file, 'r') as f:
+            data = json.loads(f.read(), object_pairs_hook=OrderedDict)
+
+    # File-like object
+    else:
+        data = json.load(json_file, object_pairs_hook=OrderedDict)
+
+    return json.dumps(data, indent=indent)
 
 
 def safe_divide(a, b):
-    """ Attempts to perform a/b and catches zero division errors to prevent crashing 
+    """ Attempts to perform a/b and catches zero division errors to prevent crashing
 
     Parameters:
     -----------
@@ -21,5 +51,5 @@ def safe_divide(a, b):
         result = a / b
     except ZeroDivisionError:
         result = 0
-    
+
     return result
