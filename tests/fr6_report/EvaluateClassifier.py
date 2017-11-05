cd36cc677b8d60cddafe5825afca46719e31873c
Zach Duey <zachduey@gmail.com>
2017-11-05 12:30:15 -0500

    Get bipolar pairs from EEG to evaluate classifier

--- tests/fr6_report/EvaluateClassifier.py
+++ tests/fr6_report/EvaluateClassifier.py
@@ -1,17 +1,21 @@
-from ReportUtils import  ReportRamTask
-from ptsa.data.readers.IndexReader import JsonIndexReader
-from os import path
 import hashlib
-from sklearn.externals import joblib
-from ReportTasks.RamTaskMethods import ModelOutput
-from random import shuffle
+import classiflib
 import numpy as np
+
+from os import path
+from random import shuffle
+from sklearn.externals import joblib
 from sklearn.metrics import roc_auc_score
+
+from ReportUtils import  ReportRamTask
+from ptsa.data.readers.IndexReader import JsonIndexReader, EEGReader
+from ReportTasks.RamTaskMethods import ModelOutput
+from ramutils.classifier.utils import reload_classifier
 
 
 class EvaluateClassifier(ReportRamTask):
-    def __init__(self,params,mark_as_completed=False):
-        super(EvaluateClassifier,self).__init__(mark_as_completed=mark_as_completed)
+    def __init__(self, params, mark_as_completed=False):
+        super(EvaluateClassifier, self).__init__(mark_as_completed=mark_as_completed)
         self.params = params
         self.pow_mat = None
         self.lr_classifier = None
@@ -86,24 +90,68 @@ class EvaluateClassifier(ReportRamTask):
     def run(self):
         subject = self.pipeline.subject
         task = self.pipeline.task
-        self.lr_classifier = self.get_passed_object('lr_classifier_full')
         events = self.get_passed_object(task+'_events')
+        monopolar_channels = self.get_passed_object("monopolar_channels")
+        bipolar_channels = self.get_passed_object("bipolar_channels")
+
+        pairs = self.get_passed_object('bipolar_pairs') # TODO: Get these from elsewhere since they do not seem to be complete
+        pairs = np.array([(int(a), int(b)) for a,b in pairs])
+
         stim_off_events = self.get_passed_object('stim_off_events')
         events = np.concatenate([events,stim_off_events]).view(np.recarray)
         self.pow_mat = self.get_passed_object('fr_stim_pow_mat')
         post_stim_pow_mat = self.get_passed_object('post_stim_pow_mat')
         self.long_pow_mat = np.concatenate([self.pow_mat,post_stim_pow_mat])
 
-        # TODO: This will change once we move to session-specific classifier?
-        all_probs = self.lr_classifier.predict_proba(self.long_pow_mat)[:, 1]
-
+        sessions = np.unique(events.session)
         non_stim = (events.type=='WORD') & (events.phase != 'STIM')
+        all_probs = np.array([])
+        for session in sessions:
+            sess_events = (events.session == session) & (non_stim)
+            pow_mat = self.long_pow_mat[sess_events]
+            recalls = events[sess_events].recalled
+
+            # Get recorded pairs from the EEG signal
+            eeg_reader = EEGReader(events=sess_events,
+                                   channels=monopolar_channels,
+                                   start_time=self.params.fr1_start_time,
+                                   end_time=self.params.fr1_end_time)
+            eeg = eeg_reader.read()
+            recorded_channels = eeg['bipolar_pairs'].values
+            recorded_channels = np.array([(int(a), int(b)) for a,b in recorded_channels]) # should be same size as power matrix?
+
+            # Load and use the session-specific classifier
+            classifier_container = reload_classifier(subject, task, session, mount_point=self.pipeline.mount_point)
+            used_pairs = classifier_container.pairs[["contact0", "contact1"]]
+            used_pairs = np.array([(int(a), int(b)) for a,b in used_pairs])
+
+            pair_mask = np.isin(recorded_channels, used_pairs) # mask of which pairs were actually used during the session
+            pow_mat = pow_mat.reshape((len(pow_mat),-1, len(self.params.freqs)))[:,pair_mask,:]
+            pow_mat.reshape((len(pow_mat),-1))
+
+            session_specific_classifier = classifier_container.classifier
+            session_specific_classifier.fit(pow_mat, recalls, classifier_container.sample_weight)
+
+            probs = session_specific_classifier.predict_proba(pow_mat)[:, 1]
+            all_probs = np.append(probs)
+
+            self.xval_output[session] = ModelOutput(recalls, probs)
+            self.xval_output[session].compute_roc()
+            self.xval_output[session].compute_tercile_stats()
+            self.xval_output[session].compute_normal_approx()
+            # Always perform in-session permutation test for single-session classifier info
+
+            self.perm_AUCs[session] = self.permuted_lolo_AUCs(events[sess_events])
+            self.pvalue[session] = np.sum(self.perm_AUCs[session] >= self.xval_output[session].auc) / float(self.perm_AUCs[session].size)
+
+        all_probs = all_probs.flatten()
+
         if not non_stim.any():
             self.xval_output = self.perm_AUCs = self.pvalue = None
         else:
-            # Need to also do this on a session-by-session basis
+            # TODO: Add Tung's AUC that is just the average of the session-specific results
             self.pow_mat = self.long_pow_mat[non_stim]
-            probs = all_probs[non_stim]
+            probs = all_probs[non_stim] # concatenate after calculating them session by session
             recalls = events[non_stim].recalled
             self.xval_output[-1] = ModelOutput(recalls, probs)
             self.xval_output[-1].compute_roc()
@@ -115,35 +163,18 @@ class EvaluateClassifier(ReportRamTask):
             nonstimlist_events = events[non_stim]
             if len(sessions)>1:
                 print 'Performing permutation test'
-
                 self.perm_AUCs[-1] = self.permuted_loso_AUCs(nonstimlist_events.session, recalls)
-
             else:
                 print 'Performing in-session permutation test'
                 self.perm_AUCs[-1] = self.permuted_lolo_AUCs(nonstimlist_events)
             self.pvalue[-1] = np.sum(self.perm_AUCs[-1] >= self.xval_output[-1].auc) / float(self.perm_AUCs[-1].size)
             print 'Perm test p-value = ', self.pvalue
 
-            for session in sessions:
-                print("Evalutation session-specific classifier for session: {}".format(session))
-                sess_events = (events.session == session) & (non_stim)
-                self.pow_mat = self.long_pow_mat[sess_events]
-                probs = all_probs[sess_events]
-                recalls = events[sess_events].recalled
-                self.xval_output[session] = ModelOutput(recalls, probs)
-                self.xval_output[session].compute_roc()
-                self.xval_output[session].compute_tercile_stats()
-                self.xval_output[session].compute_normal_approx()
-                # Always perform in-session permutation test for single-session classifier info
-                sessions = np.unique(events.session)
-                self.perm_AUCs[session] = self.permuted_lolo_AUCs(events[sess_events])
-                self.pvalue[session] = np.sum(self.perm_AUCs[session] >= self.xval_output[session].auc) / float(self.perm_AUCs[session].size)
-        
         # Slight misnomer here; these are only pre-stim in *potential*,
         # rather than in fact. This will be corrected in ComputeFRStimTable
         pre_stim_probs = all_probs[(events.type=='WORD')]
         post_stim_probs = all_probs[events.type=='STIM_OFF']
-        
+
         self.pass_object(task+'_xval_output', self.xval_output)
         self.pass_object(task+'_perm_AUCs', self.perm_AUCs)
         self.pass_object(task+'_pvalue', self.pvalue)
