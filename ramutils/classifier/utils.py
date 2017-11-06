c629c2b2ec8c835a6fa591972220497aaa32cd03
Zach Duey <zachduey@gmail.com>
2017-11-06 16:24:24 -0500

    added cross-validation function to ramutils refactor location

--- ramutils/classifier/utils.py
+++ ramutils/classifier/utils.py
@@ -1,9 +1,49 @@
 """ Utility functions used during classifier training """
 
+import os
 import h5py
 import numpy as np
+from glob import glob
 from scipy.stats.mstats import zscore
+from classiflib.container import ClassifierContainer
 
+
+def reload_classifier(subject, task, session, mount_point='/'):
+    """ Loads the actual classifier used by RAMULATOR for a particular session
+
+    Parameters:
+    -----------
+    subject: str
+        Subject ID
+    task: str
+        ex: FR5, FR6, PAL1, etc
+    session: int
+        Session number
+    mount_point: str, default '/'
+        Mount point for RHINO
+    
+    Returns:
+    --------
+    classifier_container: classiflib.container.ClassifierContainer
+
+    """
+    base_path = os.path.join(mount_point, 'data', 'eeg', subject, 'behavioral',
+                             task, 'session_{}'.format(str(session)),
+                             'host_pc')
+
+    # FIXME: this needs a data quality check to confirm that all classifiers in a session are the same!
+    # We take the final timestamped directory because in principle retrained
+    # classifiers can be different depending on artifact detection. In
+    # reality, stim sessions should never be restarted (apart from issues
+    # getting things started in the first place).
+    config_path = os.path.join(base_path, 'config_files')
+    if 'retrained_classifier' in os.listdir(config_path):
+        classifier_path = glob(os.path.join(config_path, 'retrained_classifier', '*classifier*.zip'))[0]
+    else:
+        classifier_path = glob(os.path.join(config_path, '*classifier*.zip'))[0]
+    classifier_container = ClassifierContainer.load(classifier_path)
+
+    return classifier_container
 
 def normalize_sessions(pow_mat, events):
     """ z-score powers within session
