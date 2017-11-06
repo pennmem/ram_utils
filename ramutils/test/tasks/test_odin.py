499010bdedd94338d44d6070d07c132b7880918e
Michael V. DePalatis <mike@depalatis.net>
2017-11-06 11:23:42 -0500

    Add Ramulator config task

--- ramutils/test/tasks/test_odin.py
+++ ramutils/test/tasks/test_odin.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import functools
 import json
 import os.path
 
@@ -10,6 +11,7 @@ from classiflib import ClassifierContainer
 from ramutils.parameters import StimParameters, FilePaths
 from ramutils.tasks.odin import save_montage_files, generate_ramulator_config
 from ramutils.test import Mock
+from ramutils.utils import touch
 
 
 def jsondata(s):
@@ -35,3 +37,43 @@ def test_save_montage_files(mkdir, tmpdir):
     #     path = tmpdir.join('file')
     #     path.write_binary(b'')
     #     save_montage_files(pairs, excluded, str(path))
+
+
+@pytest.mark.parametrize('experiment', ['FR6'])
+def test_generate_ramulator_config(experiment, tmpdir):
+    subject = 'R1354E'
+    classifier_path = tmpdir.join(subject).join(experiment)\
+        .join('config_files').join('{}-classifier.zip'.format(subject))
+    container = Mock(ClassifierContainer, side_effect=lambda: touch(str(classifier_path)))
+
+    Pairs = namedtuple('Pairs', 'label,anode,cathode')
+    pairs = [Pairs('1Ld1-1Ld2', 1, 2), Pairs('1Ld3-1Ld4', 3, 4)]
+
+    stim_params = [
+        StimParameters(
+            label=pair.label,
+            anode=pair.anode,
+            cathode=pair.cathode
+        )
+        for pair in pairs
+    ]
+
+    getpath = functools.partial(resource_filename, 'ramutils.test.test_data')
+    ec_conf_path = getpath('R1354E_26OCT2017L0M0STIM.csv')
+    paths = FilePaths(
+        electrode_config_file=ec_conf_path,
+
+        # Since we're not actually reading the pairs files in this test, we
+        # don't have to worry about the fact that the subjects aren't the same.
+        # All we are really doing in this test is verifying that stuff is saved.
+        pairs=getpath('R1328E_pairs.json'),
+        excluded_pairs=getpath('R1328E_excluded_pairs.json')
+    )
+
+    path = generate_ramulator_config(subject, experiment, container,
+                                     stim_params, paths, str(tmpdir)).compute()
+
+    # Ensure we saved a container
+    container.save.assert_called_once()
+
+    assert os.path.exists(path)
