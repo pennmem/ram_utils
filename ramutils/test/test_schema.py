9ed0e82c3b6da17077027955c4edbc956793cc4c
Michael V. DePalatis <mike@depalatis.net>
2017-11-06 13:20:42 -0500

    Add method to deserialize schema from HDF5
    Also removed unused private method.

--- ramutils/test/test_schema.py
+++ ramutils/test/test_schema.py
@@ -14,7 +14,6 @@ def test_to_hdf(mode, desc, tmpdir):
     class MySchema(Schema):
         x = Array(dtype=np.float64, desc=desc)
         y = Array(dtype=np.int32, desc=desc)
-
     obj = MySchema(x=np.random.random(100), y=np.random.random(100))
     filename = str(tmpdir.join('test.h5'))
 
@@ -23,3 +22,23 @@ def test_to_hdf(mode, desc, tmpdir):
     with h5py.File(filename, 'r') as hfile:
         assert_equal(hfile['/x'][:], obj.x)
         assert_equal(hfile['/y'][:], obj.y)
+
+
+def test_from_hdf(tmpdir):
+    x = np.arange(10)
+    y = np.arange(10, dtype=np.int32)
+
+    path = str(tmpdir.join('test.h5'))
+
+    with h5py.File(path, 'w') as hfile:
+        hfile.create_dataset('/x', data=x, chunks=True)
+        hfile.create_dataset('/y', data=y, chunks=True)
+
+    class MySchema(Schema):
+        x = Array(dtype=np.float64)
+        y = Array(dtype=np.int32)
+
+    instance = MySchema.from_hdf(path)
+
+    assert_equal(instance.x, x)
+    assert_equal(instance.y, y)
