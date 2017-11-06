c724adf3d7bc468c303ed48ad4c77c7a48087115
Michael V. DePalatis <mike@depalatis.net>
2017-11-06 13:03:41 -0500

    Check all config files are in zip file

--- ramutils/test/tasks/test_odin.py
+++ ramutils/test/tasks/test_odin.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
+
 from collections import namedtuple
 import functools
 import json
 import os.path
+from zipfile import ZipFile
 
 from pkg_resources import resource_string, resource_filename
 import pytest
@@ -10,7 +13,7 @@ from classiflib import ClassifierContainer
 
 from ramutils.parameters import StimParameters, FilePaths
 from ramutils.tasks.odin import save_montage_files, generate_ramulator_config
-from ramutils.test import Mock
+from ramutils.test import Mock, patch
 from ramutils.utils import touch
 
 
@@ -42,9 +45,10 @@ def test_save_montage_files(mkdir, tmpdir):
 @pytest.mark.parametrize('experiment', ['FR6'])
 def test_generate_ramulator_config(experiment, tmpdir):
     subject = 'R1354E'
-    classifier_path = tmpdir.join(subject).join(experiment)\
-        .join('config_files').join('{}-classifier.zip'.format(subject))
-    container = Mock(ClassifierContainer, side_effect=lambda: touch(str(classifier_path)))
+    classifier_path = str(tmpdir.join(subject).join(experiment)
+        .join('config_files').join('{}-classifier.zip'.format(subject)))
+
+    container = Mock(ClassifierContainer)
 
     Pairs = namedtuple('Pairs', 'label,anode,cathode')
     pairs = [Pairs('1Ld1-1Ld2', 1, 2), Pairs('1Ld3-1Ld4', 3, 4)]
@@ -59,7 +63,8 @@ def test_generate_ramulator_config(experiment, tmpdir):
     ]
 
     getpath = functools.partial(resource_filename, 'ramutils.test.test_data')
-    ec_conf_path = getpath('R1354E_26OCT2017L0M0STIM.csv')
+    ec_conf_prefix = 'R1354E_26OCT2017L0M0STIM'
+    ec_conf_path = getpath(ec_conf_prefix + '.csv')
     paths = FilePaths(
         electrode_config_file=ec_conf_path,
 
@@ -70,10 +75,16 @@ def test_generate_ramulator_config(experiment, tmpdir):
         excluded_pairs=getpath('R1328E_excluded_pairs.json')
     )
 
-    path = generate_ramulator_config(subject, experiment, container,
-                                     stim_params, paths, str(tmpdir)).compute()
+    with patch.object(container, 'save', side_effect=lambda *args, **kwargs: touch(classifier_path)):
+        path = generate_ramulator_config(subject, experiment, container,
+                                         stim_params, paths, str(tmpdir)).compute()
 
-    # Ensure we saved a container
-    container.save.assert_called_once()
+    with ZipFile(path) as zf:
+        members = zf.namelist()
 
-    assert os.path.exists(path)
+    assert 'experiment_config.json' in members
+    assert 'config_files/pairs.json' in members
+    assert 'config_files/excluded_pairs.json' in members
+    assert 'config_files/' + ec_conf_prefix + '.csv' in members
+    assert 'config_files/' + ec_conf_prefix + '.bin' in members
+    assert 'config_files/{}-classifier.zip'.format(subject) in members
