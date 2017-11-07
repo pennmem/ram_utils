36983602a008d6c1d3c962d9f12c262afa1c62cf
Michael V. DePalatis <mike@depalatis.net>
2017-11-07 14:31:24 -0500

    Add wrapper to turn ordinary function into a task

---
+++ ramutils/test/tasks/test_tasks.py
@@ -0,0 +1,12 @@
+from ramutils.tasks import make_task
+
+
+def test_make_task():
+    def diff(a, b):
+        return a - b
+
+    wrapped = make_task(diff, 2, 1)
+    assert wrapped.compute() == 1
+
+    wrapped = make_task(diff, 3, 1, cache=False)
+    assert wrapped.compute() == 2
