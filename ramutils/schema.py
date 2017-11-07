2eebf0515ee7c45338cfc477134c535f5647ff09
Michael V. DePalatis <mike@depalatis.net>
2017-11-07 10:42:22 -0500

    Cleanup

--- ramutils/schema.py
+++ ramutils/schema.py
@@ -1,8 +1,7 @@
 from __future__ import division
 
-import numpy as np
 import h5py
-from traits.api import HasTraits, Int, Float, String, Array
+from traits.api import HasTraits
 
 
 class Schema(HasTraits):
@@ -58,44 +57,3 @@ class Schema(HasTraits):
             for name in self.visible_traits():
                 setattr(self, name, hfile['/{}'.format(name)][:])
         return self
-
-
-if __name__ == "__main__":
-    import time
-
-    data = dict(
-        number=0,
-        name="FR1",
-        start=time.time() - 100,
-        end=time.time(),
-        n_words=100,
-        n_correct_words=30,
-        pc_correct_words=100/30.,
-        n_pli=10,
-        pc_pli=10/100.,
-        n_eli=0,
-        pc_eli=0.,
-        prob_recall=np.random.random((12,)),
-        prob_first_recall=np.random.random((12,)),
-        n_math=100,
-        n_correct_math=30,
-        pc_correct_math=30/100.,
-        math_per_list=10.,
-        auc=0.5,
-        fpr=np.random.random((32,)),  # FIXME: length
-        tpr=np.random.random((32,)),  # FIXME: length
-        pc_diff_from_mean=np.random.random((3,)),  # FIXME: length
-        perm_AUCs=np.random.random((32,)),  # FIXME: length
-        perm_test_pvalue=0.001,
-        jstat_thresh=0.5,
-        jstat_percentile=0.5,
-    )
-    #
-    # summary = FRSessionSummary(**data)
-    # summary.to_hdf('/tmp/summary.h5')
-    #
-    # cat_data = data.copy()
-    # cat_data['irt_within_cat'] = 0.5
-    # cat_data['irt_between_cat'] = 0.5
-    # cat_summary = CatFRSessionSummary(**cat_data)
-    # cat_summary.to_hdf('/tmp/cat_summary.h5')
