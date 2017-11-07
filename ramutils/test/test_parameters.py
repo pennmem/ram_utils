9023d86e90a2c4d99562c04f03dc5d3248db389a
Michael V. DePalatis <mike@depalatis.net>
2017-11-07 10:00:34 -0500

    Make paths relative when using __getitem__

---
+++ ramutils/test/test_parameters.py
@@ -0,0 +1,23 @@
+import functools
+import os.path
+from ramutils.parameters import FilePaths
+
+
+class TestFilePaths:
+    def test_getattr(self):
+        kwargs = dict(
+            root='/root',
+            dest='dest',
+            electrode_config_file='/tmp/ec',
+            pairs='/tmp/pairs',
+            excluded_pairs='/tmp/excluded_pairs'
+        )
+        paths = FilePaths(**kwargs)
+
+        assert paths.root == kwargs['root']
+
+        rjoin = functools.partial(os.path.join, paths.root)
+        assert paths['dest'] == rjoin(kwargs['dest'])
+        assert paths['electrode_config_file'] == rjoin(kwargs['electrode_config_file'])
+        assert paths['pairs'] == rjoin(kwargs['pairs'])
+        assert paths['excluded_pairs'] == rjoin(kwargs['excluded_pairs'])
