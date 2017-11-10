66d60010291685e33489e1f09f9a404c809fde8b
Zach Duey <zachduey@gmail.com>
2017-11-10 09:18:51 -0500

    Updates to power calculation, events process, etc. to get config generation demo running

--- demos/ramulator_config.py
+++ demos/ramulator_config.py
@@ -7,6 +7,7 @@ from pkg_resources import resource_filename
 from ptsa.data.readers import JsonIndexReader
 
 from ramutils.parameters import FilePaths, StimParameters, FRParameters
+# TODO: Don't use * for imports
 from ramutils.tasks.events import *
 from ramutils.tasks.montage import *
 from ramutils.tasks.classifier import *
@@ -16,7 +17,7 @@ from ramutils.tasks.powers import *
 getpath = functools.partial(resource_filename, 'ramutils.test.test_data')
 
 subject = 'R1354E'
-rhino = os.path.expanduser('~/mnt/rhino')
+rhino = os.path.expanduser('/Volumes/rhino')
 jr = JsonIndexReader(os.path.join(rhino, "protocols", "r1.json"))
 pairs_path = os.path.join(
     rhino, 'protocols', 'r1', 'subjects', subject,
@@ -47,24 +48,49 @@ stim_params = [
 params = FRParameters()
 
 
-### Pipeline
+### Pipeline ###
 
+#TODO: Add remove_bad_events() and remove_negative_offsets() functions to
+# process events
 fr_events = read_fr_events(jr, subject, cat=False)
 catfr_events = read_fr_events(jr, subject, cat=True)
-events = concatenate_events(fr_events, catfr_events)
+raw_events = concatenate_events(fr_events, catfr_events)
+all_events = create_baseline_events(raw_events, 1000, 29000)
+word_events = select_word_events(all_events, include_retrieval=True)
+encoding_events = select_encoding_events(word_events)
+retrieval_events = select_retrieval_events(word_events)
 
 pairs = load_pairs(pairs_path)
-# reduced_pairs = reduce_pairs(pairs, stim_params, False)
 excluded_pairs = reduce_pairs(pairs, stim_params, True)
-
 ec_pairs = generate_pairs_from_electrode_config(subject, paths)
 
-powers = compute_powers(events, params)
-classifier, xval, sample_weights = compute_classifier(events, powers, params, paths)
-container = serialize_classifier(classifier, pairs, powers, events, sample_weights, xval, subject)
+encoding_powers, good_encoding_events = compute_powers(encoding_events,
+                                                       params)
 
+retrieval_powers, good_retrieval_events = compute_powers(retrieval_events,
+                                                         params)
+
+normalized_encoding_powers = normalize_powers_by_session(encoding_powers,
+                                                         good_encoding_events)
+
+normalized_retrieval_powers = normalize_powers_by_session(retrieval_powers,
+                                                          good_retrieval_events)
+
+task_events = combine_events([good_encoding_events, good_retrieval_events])
+powers = combine_encoding_retrieval_powers(task_events,
+                                           normalized_encoding_powers,
+                                           normalized_retrieval_powers)
+
+sample_weights = get_sample_weights(task_events, params)
+classifier = train_classifier(powers, task_events, sample_weights, params)
+cross_validation_results = perform_cross_validation(classifier, powers,
+                                                    task_events, params)
+
+container = serialize_classifier(classifier, pairs, powers, task_events,
+                                 sample_weights, cross_validation_results,
+                                 subject)
 config_path = generate_ramulator_config(subject, 'FR6', container, stim_params,
                                         paths, ec_pairs, excluded_pairs)
 
-config_path.visualize()
+# config_path.visualize()
 config_path.compute()
