7a47ecd6e50f789fe29f09989692a356f28823e2
Zach Duey <zachduey@gmail.com>
2017-11-10 14:35:19 -0500

    Update tests to use new function name.

--- ramutils/test/tasks/test_montage.py
+++ ramutils/test/tasks/test_montage.py
@@ -2,14 +2,14 @@ import functools
 import json
 from pkg_resources import resource_filename
 
-from ramutils.tasks.montage import load_pairs
+from ramutils.tasks.montage import load_pairs_from_json
 
 datafile = functools.partial(resource_filename, 'ramutils.test.test_data')
 
 
 def test_load_pairs():
     filename = datafile('R1328E_pairs.json')
-    pairs = load_pairs(filename)
+    pairs = load_pairs_from_json(filename)
 
     with open(filename) as f:
         data = json.load(f)
