e711e9f273dda5937b5fde6de4fed59803412f87
Zach Duey <zachduey@gmail.com>
2017-11-10 14:02:15 -0500

    Config generation demo pipeline working.

--- ramutils/tasks/powers.py
+++ ramutils/tasks/powers.py
@@ -39,7 +39,29 @@ def combine_encoding_retrieval_powers(events, encoding_powers,
     powers[retrieval_mask, ...] = retrieval_powers
     return powers
 
+
 @task()
 def normalize_powers_by_session(pow_mat, events):
     normalized_pow_mat = normalize_powers_by_session_core(pow_mat, events)
     return  normalized_pow_mat
+
+
+@task()
+def reduce_powers(powers, mask, n_frequencies):
+    """ Select a subset of electrodes from the power matrix based on mask
+
+    :param powers:
+    :param mask:
+    :param n_frequencies:
+    :return:
+
+    """
+
+    # Reshape into 3-dimensional array (n_events, n_electrodes, n_frequencies)
+    reduced_powers = powers.reshape((len(powers), -1, n_frequencies))
+    reduced_powers = reduced_powers[:, mask, :]
+
+    # Reshape back to 2D representation so it can be used as a feature matrix
+    reduced_powers = reduced_powers.reshape((len(reduced_powers), -1))
+
+    return reduced_powers
