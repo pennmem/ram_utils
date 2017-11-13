df749175dea1aee6b996d37be92d9a4ca2a9728e
Michael V. DePalatis <mike@depalatis.net>
2017-11-13 13:16:45 -0500

    Move supported experiments to constants module

---
+++ ramutils/constants.py
@@ -0,0 +1,22 @@
+EXPERIMENTS = {
+    'record_only': [
+        'FR1',
+        'catFR1',
+        'PAL1'
+    ],
+    'ps': [
+        # 'PS2',
+        'PS4_FR5',
+        'PS4_catFR5'
+    ],
+    'closed_loop': [
+        'FR3',
+        'catFR4',
+        'PAL3',
+        'FR5',
+        'catFR5',
+        'PAL5',
+        'FR6',
+        'catFR6',
+    ]
+}
