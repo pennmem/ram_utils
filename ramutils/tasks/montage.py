64801f36ccdb2f7f1ed1926712d7473785aaa115
Michael V. DePalatis <mike@depalatis.net>
2017-11-13 13:19:57 -0500

    Don't cache trivial tasks

--- ramutils/tasks/montage.py
+++ ramutils/tasks/montage.py
@@ -33,7 +33,7 @@ def _atlas_location(bp_data):
     return '--'
 
 
-@task()
+@task(cache=False)
 def load_pairs_from_json(path):
     """Load pairs.json.
 
@@ -114,7 +114,7 @@ def generate_pairs_for_classifier(pairs, excluded_pairs):
     used_pairs = {
         key: value for key, value in pairs.items()
         if key not in excluded_pairs
-        }
+    }
 
     pairs = np.rec.fromrecords([(item['channel_1'], item['channel_2'],
                                  pair.split('-')[0], pair.split('-')[1])
@@ -126,7 +126,7 @@ def generate_pairs_for_classifier(pairs, excluded_pairs):
     return pairs
 
 
-@task()
+@task(cache=False)
 def get_used_pair_mask(all_pairs, excluded_pairs):
     """ Get a mask of whether to include a pair in all_pairs based on
     excluded pairs
