979167a1bfb11cfda7711c54ce8818d98f45e64b
Michael V. DePalatis <mike@depalatis.net>
2017-11-20 12:56:39 -0500

    Build config JSON file from a dict
    Using a template for a JSON file is kind of silly, after all.

--- ramutils/tasks/odin.py
+++ ramutils/tasks/odin.py
@@ -13,8 +13,6 @@ try:
 except ImportError:
     pass
 
-from tornado.template import Template
-
 from bptools.transform import SeriesTransformation
 from classiflib import ClassifierContainer
 
@@ -84,6 +82,103 @@ def generate_pairs_from_electrode_config(subject, paths):
         return pairs_from_ec
 
 
+def _make_ramulator_config_json(subject, experiment, electrode_config_file,
+                                stim_params, classifier_file=None,
+                                classifier_version=None):
+    """Create the Ramulator ``experiment_config.json`` file.
+
+    Parameters
+    ----------
+    subject : str
+    experiment : str
+    electrode_config_file : str
+    stim_params : dict
+    classifier_file : str
+    classifier_version : str
+
+    Returns
+    -------
+    str
+
+    """
+    config = OrderedDict()
+    config['subject'] = subject
+    config['experiment'] = experiment
+
+    # experiment_specific_data section
+    esd = OrderedDict()
+
+    if experiment != 'AmplitudeDetermiation':
+        esd['classifier_file'] = os.path.join('config_files', classifier_file)
+        esd['classifier_version'] = classifier_version
+
+    if experiment == 'AmplitudeDetermination':
+        esd['stim_electrode_pairs'] = [label for label in stim_params]
+    else:
+        esd['stim_channels'] = {}
+        for i, label in enumerate(stim_params):
+            if 'PS4' in experiment:
+                esd['stim_channels'][label] = {
+                    'min_stim_amplitude': stim_params[label]['min_stim_amplitude'],
+                    'max_stim_amplitude': stim_params[label]['max_stim_amplitude']
+                }
+            else:
+                esd['stim_channels'][label] = {
+                    'stim_amplitude': stim_params[label]['stim_amplitude']
+                }
+            esd['stim_channels'][label]['stim_frequency'] = stim_params[label]['stim_frequency']
+            esd['stim_channels'][label]['stim_duration'] = stim_params[label]['stim_duration']
+
+    esd['random_stim_prob'] = False
+    esd['save_debug_output'] = True
+    config['experiment_specific_data'] = esd
+
+    # FIXME: values below shouldn't be hardcoded
+    config['experiment_specs'] = {
+        "version": "3.0.0",
+        "experiment_type": experiment,
+        "biomarker_sample_start_time_offset": 0,
+        "biomarker_sample_time_length": 1366,
+        "buffer_time": 1365,
+        "stim_duration": 500,
+        "freq_min": 6,
+        "freq_max": 180,
+        "num_freqs": 8,
+        "num_items": 300,
+    }
+
+    # FIXME: values below shouldn't be hardcoded
+    if 'PS4' in experiment:
+        config['experiment_specs'].update({
+            "retrieval_biomarker_sample_start_time_offset": 0,
+            "retrieval_biomarker_sample_time_length": 525,
+            "retrieval_buffer_time": 524,
+            "post_stim_biomarker_sample_time_length": 500,
+            "post_stim_buffer_time": 499,
+            "post_stim_wait_time":100,
+        })
+
+    config.update({
+        "biomarker_threshold": 0.5,
+        "electrode_config_file": "config_files/{}".format(electrode_config_file),
+        "montage_file": "config_files/pairs.json",
+        "excluded_montage_file": "config_files/excluded_pairs.json",
+        "global_settings": {
+            "data_dir": "SET_AUTOMATICALLY_AT_A_RUNTIME",
+            "experiment_config_filename": "SET_AUTOMATICALLY_AT_A_RUNTIME",
+            "plot_fps": 5,
+            "plot_window_length": 20000,
+            "plot_update_style": "Sweeping",
+            "max_session_length": 120,
+            "sampling_rate": 1000,
+            "odin_lib_debug_level": 0,
+            "connect_to_task_laptop": True
+        }
+    })
+
+    return json.dumps(config, indent=2)
+
+
 @task(cache=False)
 def generate_ramulator_config(subject, experiment, container, stim_params,
                               paths, pairs=None, excluded_pairs=None,
@@ -140,20 +235,9 @@ def generate_ramulator_config(subject, experiment, container, stim_params,
     classifier_path = os.path.join(config_files_dir, '{}-classifier.zip'.format(subject))
     ec_prefix, _ = os.path.splitext(paths.electrode_config_file)
 
-    template_filename = os.path.join(
-        os.path.dirname(__file__), 'templates', 'ramulator_config.json')
-
-    with open(template_filename, 'r') as f:
-        experiment_config_template = Template(f.read())
-
-    experiment_config_content = experiment_config_template.generate(
-        subject=subject,
-        experiment=experiment,
-        classifier_file='config_files/{}'.format(os.path.basename(classifier_path)),
-        classifier_version=CLASSIFIER_VERSION,
-        stim_params_dict=stim_dict,
-        electrode_config_file='config_files/{}'.format(os.path.basename(ec_prefix + '.bin')),
-        biomarker_threshold=0.5
+    experiment_config_content = _make_ramulator_config_json(
+        subject, experiment, os.path.basename(ec_prefix + '.bin'), stim_dict,
+        os.path.basename(classifier_path), CLASSIFIER_VERSION
     )
 
     with open(os.path.join(config_dir_root, 'experiment_config.json'), 'w') as f:
