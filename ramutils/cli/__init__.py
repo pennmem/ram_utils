7cf6e9a8c75d24f96aa2cf272e04b27316479e01
Michael V. DePalatis <mike@depalatis.net>
2017-11-21 13:47:39 -0500

    Use separate argument for cache dir
    MacOS is weird and for some reason doesn't like the cache directory
    being located on an sshfs-mounted filesystem even though it seems
    perfectly fine with reading files. This is apparently a problem
    only related to joblib's caching.

--- ramutils/cli/__init__.py
+++ ramutils/cli/__init__.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 import os.path
+import tempfile
+
 from ramutils.constants import EXPERIMENTS
 
 
@@ -21,10 +23,19 @@ def make_parser(description, allowed_experiments=sum([exps for exps in EXPERIMEN
     -------
     parser : ArgumentParser
 
+    Notes
+    -----
+    Paths are relative to the root argument except for cachedir when specified.
+    When not given, cachedir will default to::
+
+        os.path.join(tempfile.gettempdir(), 'ramutils')
+
     """
     parser = ArgumentParser(description=description)
     parser.add_argument('--root', default='/', help='path to rhino root')
     parser.add_argument('--dest', '-d', default='.', help='directory to write output to')
+    parser.add_argument('--cachedir', '-c', default=os.path.join(tempfile.gettempdir(), 'ramutils'),
+                        help='absolute path for caching dir')
     parser.add_argument('--subject', '-s', required=True, type=str, help='subject ID')
     parser.add_argument('--force-rerun', action='store_true', help='force re-running all tasks')
     parser.add_argument('--experiment', '-x', required=True, type=str,
