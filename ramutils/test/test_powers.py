4c928f27fd53fd7fdb8249dfe6591cd1bc96f486
Zach Duey <zachduey@gmail.com>
2017-11-27 17:11:26 -0500

    Calculate powers using partition-specific start, stop, and buffer times.

---
+++ ramutils/test/test_powers.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+# Cases: EEG from monopolar, mixed mode, and bipolar. EEG with removed bad
+# data and without. Log powers true and false
+def test_compute_single_session_powers():
+    return
+
+# Cases: Bipolar_pairs == none and not none
+def test_compute_powers():
+    return
+
+
+# No reductions, some reductions, all reductions
+def test_reduce_powers():
+    return
+
+# Not sure how much there is to test here
+def test_normalize_powers_by_session():
+    return
