7fe0c7aff81b779a9eab40125dfb67f22b6d7839
Michael V. DePalatis <mike@depalatis.net>
2017-12-05 11:23:53 -0500

    Make plots for classifier performance

--- ramutils/reports/static/plots.js
+++ ramutils/reports/static/plots.js
@@ -3,6 +3,12 @@
  */
 
 var ramutils = (function (mod, Plotly) {
+  /**
+   * Make the axes options object.
+   * @param {String} xlabel
+   * @param {String} ylabel
+   * @return {Object}
+   */
   mod.makeAxesOptions = function (xlabel, ylabel) {
     return {
       xaxis: {
@@ -41,7 +47,63 @@ var ramutils = (function (mod, Plotly) {
       layout.xaxis.range = [1, 12];
       layout.yaxis.range = [0, 1];
 
-      Plotly.plot('serialpos-placeholder', data, layout);
+      Plotly.plot('serialpos-plot-placeholder', data, layout);
+    },
+
+    /**
+     * Classifier performance plots: ROC curve and tercile plot.
+     * @param {Array} fpr - False positive rate
+     * @param {Array} tpr - True positive rate
+     * @param {Number} low
+     * @param {Number} middle
+     * @param {Number} high
+     */
+    classifierPerformance: function (fpr, tpr, low, middle, high) {
+      const data = [
+        {
+          x: fpr,
+          y: tpr,
+          type: 'scatter',
+          mode: 'lines',
+          name: 'ROC'
+        },
+        {
+          x: ['low', 'middle', 'high'],
+          y: [low, middle, high],
+          xaxis: 'x2',
+          yaxis: 'y2',
+          type: 'bar',
+          name: 'Tercile'
+        }
+      ];
+
+      const layout = {
+        xaxis: {
+          title: 'False positive rate',
+          domain: [0, 0.45],
+          range: [0, 1]
+        },
+        yaxis: {
+          title: 'True positive rate',
+          range: [0, 1]
+        },
+        xaxis2: {
+          title: 'Tercile of classifier estimate',
+          domain: [0.55, 1]
+        },
+        yaxis2: {
+          title: 'Recall change from mean (%)',
+          anchor: 'x2'
+        },
+
+        showlegend: false,
+
+        // FIXME: responsive and square aspect ratio?
+        width: 1000,
+        height: 500
+      };
+
+      Plotly.plot('classifier-performance-plot-placeholder', data, layout);
     }
   };
 
