b4b85d6b6056caf4d2d651bc4eb88f9c744eff33
Zach Duey <zachduey@gmail.com>
2017-12-06 15:15:19 -0500

    WIP: report pipeline code

--- ramutils/pipelines/report.py
+++ ramutils/pipelines/report.py
@@ -4,6 +4,8 @@
 from ramutils.tasks import *
 
 
+# TODO: Are record-only report different enough from stim report that we should have two different pipelines and just
+# unify in the command line interface?
 def make_report(subject, experiment, paths, stim_params=None, classifier_container=None,
                 exp_params=None, sessions=None, vispath=None):
     """Run a report.
@@ -41,16 +43,15 @@ def make_report(subject, experiment, paths, stim_params=None, classifier_contain
     report rather than the report itself.
 
     """
-    # If classifier container is provided, load as much as possible from that
-
     # Note: All of these pairs variables are of type OrderedDict, which is
     # crucial for preserving the initial order of the electrodes in the
     # config file
     ec_pairs = generate_pairs_from_electrode_config(subject, paths)
+    excluded_pairs = reduce_pairs(ec_pairs, stim_params, True)
+    used_pair_mask = get_used_pair_mask(ec_pairs, excluded_pairs)
+    final_pairs = generate_pairs_for_classifier(ec_pairs, excluded_pairs)
+
     pairs_metadata_table = generate_montage_metadata_table(subject, ec_pairs, root=paths.root)
-    #excluded_pairs = reduce_pairs(ec_pairs, stim_params, True)
-    #used_pair_mask = get_used_pair_mask(ec_pairs, excluded_pairs)
-    #final_pairs = generate_pairs_for_classifier(ec_pairs, excluded_pairs)
 
     if classifier_container is not None:
         # Load task events, reduced powers, trained classifier, etc all from the given serialized classifier container
@@ -58,20 +59,12 @@ def make_report(subject, experiment, paths, stim_params=None, classifier_contain
         events = classifier_container.events
 
     kwargs = exp_params.to_dict()
-    events = preprocess_events(subject,
-                               experiment,
-                               kwargs['baseline_removal_start_time'],
-                               kwargs['retrieval_time'],
-                               kwargs['empty_epoch_duration'],
-                               kwargs['pre_event_buf'],
-                               kwargs['post_event_buf'],
-                               encoding_only=kwargs['encoding_only'],
-                               combine_events=kwargs['combine_events'],
-                               root=paths.root)
+    # Use a different function here? generate_event_subsets() -- We need all events in order to create MathSummary
+    events = preprocess_events(subject, experiment, kwargs['baseline_removal_start_time'], kwargs['retrieval_time'],
+                               kwargs['empty_epoch_duration'], kwargs['pre_event_buf'], kwargs['post_event_buf'],
+                               sessions=sessions, combine_events=kwargs['combine_events'], root=paths.root)
 
-    # FIXME: If PTSA is updated to not remove events behind this scenes, this
-    # won't be necessary. Or, if we can remove bad events before passing to
-    # compute powers, then we won't have to catch the events
+    # Both powers and task_events need to be saved out somewhere
     powers, task_events = compute_normalized_powers(events,
                                                     **kwargs)
 
@@ -81,33 +74,41 @@ def make_report(subject, experiment, paths, stim_params=None, classifier_contain
                                                 kwargs['freqs'],
                                                 hfa_cutoff=kwargs['hfa_cutoff'])
 
-    #reduced_powers = reduce_powers(powers, used_pair_mask, len(kwargs['freqs']))
+    # Create another task here called build_classifier(events, powers) and returning a trained classifier
+    # that can be passed to cross-validation. Then, we can train a set of classifiers on an arbitrary set of
+    # events/powers
 
-    #sample_weights = get_sample_weights(task_events, **kwargs)
+    reduced_powers = reduce_powers(powers, used_pair_mask, len(kwargs['freqs']))
 
-    #classifier = train_classifier(reduced_powers,
-    #                              task_events,
-    #                             sample_weights,
-    #                              kwargs['C'],
-    #                              kwargs['penalty_type'],
-    #                              kwargs['solver'])
+    sample_weights = get_sample_weights(task_events, **kwargs)
 
-    #cross_validation_results = perform_cross_validation(classifier,
-    #                                                    reduced_powers,
-    #                                                    task_events,
-    #                                                    kwargs['n_perm'],
-    #                                                    **kwargs)
+    # For FR1/catFR1 report, we need to be able to train the encoding only and joint classifier for comparison
+    # This means different event subsets as well as different power subsets
+    classifier = train_classifier(reduced_powers,
+                                  task_events,
+                                  sample_weights,
+                                  kwargs['C'],
+                                  kwargs['penalty_type'],
+                                  kwargs['solver'])
 
+    cross_validation_results = perform_cross_validation(classifier,
+                                                        reduced_powers,
+                                                        task_events,
+                                                        kwargs['n_perm'],
+                                                        **kwargs)
 
-
-    # generate summaries for each session
-    # FIXME: encoding events or all events?
-    # session_summaries = [
-    #     summarize_session(events[events.session == session])
-    #     for session in sessions
-    # ]
+    if sessions is None:
+        session_summaries = [
+            summarize_session(events[events.session == session])
+            for session in sessions]
+    else:
+        session_summaries = [summarize_session(events)]
 
     if vispath is not None:
         pass
 
-    return delta_hfa_table.compute()
+    math_summaries = []
+    classifier_summaries = [cross_validation_results]
+    report_path = build_static_report(session_summaries, math_summaries, classifier_summaries, delta_hfa_table)
+
+    return report_path.compute()
