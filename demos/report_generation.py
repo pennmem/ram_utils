454c01dbdd1c8477a6f0af7f5b25d86a67e21846
Zach Duey <zachduey@gmail.com>
2017-12-11 15:42:26 -0500

    Report gen sample for subject with bipolar recording and stim sessions

--- demos/report_generation.py
+++ demos/report_generation.py
@@ -6,6 +6,7 @@ from pkg_resources import resource_filename
 
 from ramutils.parameters import FilePaths, FRParameters
 from ramutils.pipelines.report import make_report
+from ramutils.pipelines.ramulator_config import make_stim_params
 
 from ramutils.tasks import memory
 
@@ -14,22 +15,25 @@ memory.cachedir = "/Users/zduey/tmp/"
 
 getpath = functools.partial(resource_filename, 'ramutils.test.test_data')
 
-subject = 'R1354E'
+subject = 'R1345D'
 rhino = os.path.expanduser('/Volumes/rhino')
 pairs_path = os.path.join(
-    rhino, 'protocols', 'r1', 'subjects', subject,
+    'protocols', 'r1', 'subjects', subject,
     'localizations', str(0),
     'montages', str(0),
     'neuroradiology', 'current_processed', 'pairs.json')
 
 paths = FilePaths(
     root='/Volumes/RHINO/',
-    electrode_config_file='/scratch/system3_configs/ODIN_configs/R1354E'
-                          '/R1354E_26OCT2017L0M0STIM.csv',
+    electrode_config_file='/scratch/system3_configs/ODIN_configs/R1345D'
+                          '/R1345D_10OCT2017L0M0STIM.csv',
     pairs=pairs_path,
-    dest='scratch/zduey/sample_fr1_reports'
+    dest='scratch/zduey/sample_fr5_reports'
 )
 
 params = FRParameters()
-params.combine_events = False
-make_report(subject, "FR1", paths, sessions=[0], exp_params=params)
+stim_params = make_stim_params(subject, ['LTG27', 'LTG20'], ['LTG28', 'LTG21'],
+                               target_amplitudes=[1.0, 1.0],
+                               root=paths.root)
+make_report(subject, "FR5", paths, exp_params=params, stim_params=stim_params,
+            sessions=[0], joint_report=False)
