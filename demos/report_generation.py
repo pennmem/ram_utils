1f315b86eab5a71fb1761e422c6d4aa70505de5f
Zach Duey <zachduey@gmail.com>
2017-12-13 17:23:04 -0500

    Updated default demos

--- demos/report_generation.py
+++ demos/report_generation.py
@@ -15,7 +15,7 @@ memory.cachedir = "/Users/zduey/tmp/"
 
 getpath = functools.partial(resource_filename, 'ramutils.test.test_data')
 
-subject = 'R1345D'
+subject = 'R1374T'
 rhino = os.path.expanduser('/Volumes/rhino')
 pairs_path = os.path.join(
     'protocols', 'r1', 'subjects', subject,
@@ -25,15 +25,12 @@ pairs_path = os.path.join(
 
 paths = FilePaths(
     root='/Volumes/RHINO/',
-    electrode_config_file='/scratch/system3_configs/ODIN_configs/R1345D'
-                          '/R1345D_10OCT2017L0M0STIM.csv',
+    electrode_config_file='/scratch/system3_configs/ODIN_configs/R1374T'
+                          '/R1374T_12DEC2017L0M0STIM.csv',
     pairs=pairs_path,
     dest='scratch/zduey/samplefr1_reports'
 )
 
 params = FRParameters()
-stim_params = make_stim_params(subject, ['LTG27', 'LTG20'], ['LTG28', 'LTG21'],
-                               target_amplitudes=[1.0, 1.0],
-                               root=paths.root)
-make_report(subject, "FR1", paths, exp_params=params, stim_params=stim_params,
-            sessions=[0], joint_report=False)
+make_report(subject, "catFR1", paths, exp_params=params, stim_params=None,
+            joint_report=True)
