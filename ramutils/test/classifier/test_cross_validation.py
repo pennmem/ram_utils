ae6fa39d7469f2e264f9d8f761fb423e937b17a4
Michael V. DePalatis <mike@depalatis.net>
2017-12-15 09:52:50 -0500

    Skip unimplemented tests, mark xval tests as slow

--- ramutils/test/classifier/test_cross_validation.py
+++ ramutils/test/classifier/test_cross_validation.py
@@ -1,28 +1,22 @@
 import functools
-
 from pkg_resources import resource_filename
+import pytest
 
 
 datafile = functools.partial(resource_filename,
                              'ramutils.test.test_data.input')
 
 
-def test_perform_lolo_cross_validation():
-    return
+@pytest.mark.skip(reason='not implemented')
+class TestCrossValidation:
+    def test_perform_lolo_cross_validation(self):
+        return
 
+    def test_perform_loso_cross_validation(self):
+        return
 
-def test_perform_loso_cross_validation():
-    return
+    def test_permuted_lolo_cross_validation(self):
+        return
 
-
-def test_permuted_lolo_cross_validation():
-    return
-
-
-def test_permuted_loso_cross_validation():
-    return
-
-
-
-
-
+    def test_permuted_loso_cross_validation(self):
+        return
