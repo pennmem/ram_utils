1202cdf6c9f76284c9331806c19b0028e0519b73
Michael V. DePalatis <mike@depalatis.net>
2017-12-19 11:12:43 -0500

    Update conda_update.sh
    Only `set -e` after trying to remove the `ramutils` environment.

--- maint/conda_update.sh
+++ maint/conda_update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-set -e
 conda env remove -n ramutils -y
+set -e
 conda create -n ramutils python=3
 source activate ramutils
 conda install -y -c pennmem --file=requirements.txt
