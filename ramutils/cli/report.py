736f0b933cde42ee75e9272ebf626b6d60826263
Michael V. DePalatis <mike@depalatis.net>
2017-12-20 10:45:45 -0500

    Add excluded contacts command-line option

--- ramutils/cli/report.py
+++ ramutils/cli/report.py
@@ -1,11 +1,18 @@
-"""Script for generating reports."""
+"""Script for generating reports.
+
+The ``--excluded-contacts`` option is intended to be used in non-stim
+experiments to see how a classifier would perform when excluding the contacts
+chosen for stimulation.
+
+"""
 
 import os.path as osp
 
 from ramutils.cli import make_parser, configure_caching
 from ramutils.exc import UnsupportedExperimentError
 from ramutils.log import get_logger
-from ramutils.parameters import FilePaths, FRParameters
+from ramutils.montage import make_stim_params
+from ramutils.parameters import FilePaths, FRParameters, StimParameters
 from ramutils.pipelines.report import make_report
 
 parser = make_parser("Generate a report")
@@ -13,6 +20,8 @@ parser.add_argument('--sessions', '-S', nargs='+',
                     help='sessions to read data from (default: use all)')
 parser.add_argument("--retrain", "-R", action="store_true",
                     help="retrain classifier rather than loading from disk")
+parser.add_argument('--excluded-contacts', '-E', nargs='+',
+                    help='contacts to exclude from classifier')
 
 logger = get_logger("reports")
 
@@ -27,8 +36,15 @@ def main(input_args=None):
         dest=args.dest,
     )
 
-    # FIXME: Construct stim parameters
-    stim_params = None
+    # Stim params (used in make_report below) is really just used for excluding
+    # pairs.
+    if args.excluded_contacts is not None:
+        stim_params = make_stim_params(args.subject,
+                                       anodes=args.excluded_contacts,
+                                       cathodes=args.excluded_contacts,
+                                       root=paths.root)
+    else:
+        stim_params = []
 
     # Extract sessions
     if args.sessions is not None:
@@ -37,13 +53,14 @@ def main(input_args=None):
         sessions = None
 
     if 'FR' in args.experiment:
-        exp_params = FRParameters
+        exp_params = FRParameters()
     elif 'PAL' in args.experiment:
         raise NotImplementedError("PAL experiments are not supported yet")
     else:
         raise UnsupportedExperimentError("Unsupported experiment: " + args.experiment)
 
     # Generate report!
+    # FIXME: stim_params should be called something different/just be a list of contacts to exclude
     path = make_report(
         subject=args.subject,
         experiment=args.experiment,
