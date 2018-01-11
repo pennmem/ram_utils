94e06e0b06efec3ea3d0916a2d6dc89c490bd0da
Zach Duey <zachduey@gmail.com>
2018-01-11 12:22:05 -0500

    Placeholder for test script that will be run nightly

--- maint/run_tests.sh
+++ maint/run_tests.sh
@@ -1,2 +1,29 @@
 #!/usr/bin/env bash
-python -m pytest --cov=ramutils --cov-report html "$@"
+conda env remove -n ramutils_test -y
+set -e
+conda create -y -n ramutils_test python=3
+source activate ramutils_test
+conda install -y -c pennmem -c conda-forge --file=requirements.txt
+
+git pull origin master
+python setup.py install
+python -m pytest ramutils/ --cov=ramutils --cov-report html --html=report.html --self-contained-html --rhino-root=/ --output-dest=/scratch/zduey
+
+zip coverage.zip htmlcov/
+echo "Full test suite finished running" | mail -a coverage.zip -a report.html -s "Python 3.6 Test Results" zachduey@gmail.com
+
+conda env remove -n ramutils_test_27 -y
+set -e
+conda create -y -n ramutils_test_27 python=2.7
+source activate ramutils_test_27
+conda install -y -c pennmem -c conda-forge --file=requirements.txt
+
+python -m pytest ramutils/ --cov=ramutils --cov-report html --html=report.html --self-contained-html --rhino-root=/ --output-dest=/scratch/zduey
+
+# Zip the htmlcov/ folder and email
+zip coverage.zip htmlcov/
+echo "Full test suite finished running" | mail -a coverage.zip -a report.html -s "Python 2.7 Test Results" zachduey@gmail.com
+
+
+
+
