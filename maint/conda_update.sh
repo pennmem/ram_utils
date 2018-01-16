4fd6a1c2c6ff0e779e1d76e27ecc9030177aeb8f
Michael V. DePalatis <mike@depalatis.net>
2018-01-16 15:52:50 -0500

    Use legacy Python in conda_update.sh
    Later we'll use a conda package to update the RAM_clinical
    environment instead.

--- maint/conda_update.sh
+++ maint/conda_update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 conda env remove -n ramutils -y
 set -e
-conda create -y -n ramutils python=3
+conda create -y -n ramutils python=2.7
 source activate ramutils
 conda install -y -c pennmem -c conda-forge --file=requirements.txt
 python setup.py install
