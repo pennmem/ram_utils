26a755b25ab751a05f27a38ddb0e67935004d10e
Michael V. DePalatis <mike@depalatis.net>
2018-01-19 10:55:36 -0500

    Use conda convert command

--- maint/build.py
+++ maint/build.py
@@ -3,15 +3,39 @@
 from __future__ import print_function
 
 from argparse import ArgumentParser
+import glob
 import os
+import platform
+import shlex
 import shutil
 from subprocess import check_call
+import sys
 
 parser = ArgumentParser()
 parser.add_argument("--no-clean", action="store_true",
                     help="don't remove existing build dir")
+parser.add_argument("--no-build", action="store_true",
+                    help="don't build conda packages")
+parser.add_argument("--no-convert", action="store_true",
+                    help="don't run conda convert")
 parser.add_argument("--python", "-p", nargs="+", default=["2.7", "3.6"],
                     help="python versions to build for (otherwise build all)")
+
+
+def convert():
+    """Convert conda packages to other platforms."""
+    os_name = {
+        'darwin': 'osx',
+        'win32': 'win',
+        'linux': 'linux'
+    }[sys.platform]
+    dirname = '{}-{}'.format(os_name, platform.architecture()[0][:2])
+    files = glob.glob('build/{}/*.tar.bz2'.format(dirname))
+
+    for filename in files:
+        convert_cmd = "conda convert {} -p all -o build/".format(filename)
+        print(convert_cmd)
+        check_call(shlex.split(convert_cmd))
 
 
 if __name__ == "__main__":
@@ -30,16 +54,20 @@ if __name__ == "__main__":
         'pennmem',
     ]
 
-    for pyver in args.python:
-        build_cmd = [
-            "conda", "build",
-            "--output-folder=build/",
-            "--python", pyver,
-        ]
+    if not args.no_build:
+        for pyver in args.python:
+            build_cmd = [
+                "conda", "build",
+                "--output-folder=build/",
+                "--python", pyver,
+            ]
 
-        for chan in channels:
-            build_cmd += ['-c', chan]
-        build_cmd += ["conda.recipe"]
+            for chan in channels:
+                build_cmd += ['-c', chan]
+            build_cmd += ["conda.recipe"]
 
-        print(' '.join(build_cmd))
-        check_call(build_cmd)
+            print(' '.join(build_cmd))
+            check_call(build_cmd)
+
+    if not args.no_convert:
+        convert()
