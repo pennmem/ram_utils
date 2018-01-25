b8210c1165b8f77d75e51539697f101319c29117
Zach Duey <zachduey@gmail.com>
2018-01-25 17:07:05 -0500

    Updated test script to work with rhino

--- maint/run_tests.sh
+++ maint/run_tests.sh
@@ -1,29 +1,37 @@
 #!/usr/bin/env bash
-conda env remove -n ramutils_test -y
-set -e
-conda create -y -n ramutils_test python=3
-source activate ramutils_test
-conda install -y -c pennmem -c conda-forge --file=requirements.txt
+#conda env remove -n ramutils_test -y
+#set -e
+#echo "Creating python 3 environment"
+#conda create -y -n ramutils_test python=3
+#source activate ramutils_test
+#conda install -y -c pennmem -c conda-forge --file=requirements.txt
+#
+#echo "Pulling master branch from remote repository"
+#git pull origin master
+#
+## If any tests fail, non-zero error code returned by pytest, so allow shell script to contiue
+#echo "Running full test suite on python 3.6"
+#set +e
+#python -m pytest ramutils/ --cov=ramutils --cov-report html --html=report.html --self-contained-html --rhino-root=/ --output-dest=/scratch/zduey/nightly_build/python_36/
+#set -e
+#
+#zip coverage.zip htmlcov/
+#echo "Full test suite finished running" | mail -a coverage.zip -a report.html -s "Python 3.6 Test Results" zachduey@gmail.com
 
-git pull origin master
-python setup.py install
-python -m pytest ramutils/ --cov=ramutils --cov-report html --html=report.html --self-contained-html --rhino-root=/ --output-dest=/scratch/zduey
-
-zip coverage.zip htmlcov/
-echo "Full test suite finished running" | mail -a coverage.zip -a report.html -s "Python 3.6 Test Results" zachduey@gmail.com
-
+echo "Creating python 2.7 environment"
+set +e
 conda env remove -n ramutils_test_27 -y
 set -e
 conda create -y -n ramutils_test_27 python=2.7
 source activate ramutils_test_27
 conda install -y -c pennmem -c conda-forge --file=requirements.txt
 
-python -m pytest ramutils/ --cov=ramutils --cov-report html --html=report.html --self-contained-html --rhino-root=/ --output-dest=/scratch/zduey
+echo "Running full test suite on python 2.7"
+set +e
+python -m pytest ramutils/ --cov=ramutils --cov-report html --html=report.html --self-contained-html --rhino-root=/ --output-dest=/scratch/zduey/nightly_build/python_27/
+set -e
 
 # Zip the htmlcov/ folder and email
 zip coverage.zip htmlcov/
 echo "Full test suite finished running" | mail -a coverage.zip -a report.html -s "Python 2.7 Test Results" zachduey@gmail.com
-
-
-
 
