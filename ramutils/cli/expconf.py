0a056308a822122bc409d657556554b6f3be4990
Zach Duey <zachduey@gmail.com>
2018-03-07 15:29:16 -0500

    Added back 'debugging issues found in the wild' option for Mike

--- ramutils/cli/expconf.py
+++ ramutils/cli/expconf.py
@@ -190,3 +190,54 @@ def create_expconf(input_args=None):
     if warnings is not None:
         logger.info(warnings)
 
+if __name__ == "__main__":
+    # create_expconf()
+
+    args = ['--root', '~/mnt/rhino', '-d', 'scratch/depalati']
+
+    # create_expconf(args + [
+    #     '-s', 'R1383J', '-x', 'FR5',
+    #     '--anodes', 'LB7',
+    #     '--cathodes', 'LB8',
+    #     '--target-amplitudes', '0.5'
+    # ])
+
+    create_expconf(args + [
+        '-s', 'R1111M', '-x', 'DBOY1',
+        '--default-area', '5', '--use-common-reference',
+    ])
+
+    # create_expconf(args + [
+    #     '-s', 'R1384J', '-x', 'PS5_CatFR',
+    #     '--anodes', 'LF7',
+    #     '--cathodes', 'LF8',
+    #     '--min-amplitudes', '0.1',
+    #     '--max-amplitudes', '0.5',
+    #     '--trigger-pairs', 'RPMS1_RPMS2',
+    #     '--default-area', '5.024',
+    # ])
+
+    # create_expconf(args + [
+    #     '-s', 'R1385E',
+    #     '-x', 'CatFR5',
+    #     '--electrode-config-file',
+    #     'scratch/system3_configs/ODIN_configs/R1385E/R1385E_15JAN2018L0M0STIM.csv',
+    #     '--anodes', '3LD8',
+    #     '--cathodes', '3LD9',
+    #     '--target-amplitudes', '0.5'
+    # ])
+
+    # create_expconf(args + [
+    #     '-s',
+    #     'R1385E',
+    #     '-x',
+    #     'CatFR5',
+    #     '--electrode-config-file',
+    #     'scratch/system3_configs/ODIN_configs/R1385E/R1385E_15JAN2018L0M0STIM.csv',
+    #     '--anodes',
+    #     '3LD8',
+    #     '--cathodes',
+    #     '3LD9',
+    #     '--target-amplitudes',
+    #     '0.5'
+    # ])
