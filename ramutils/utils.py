2ab5afe3b4d832d6170b9f0796f780c1e6028071
leond <lad258@cornell.edu>
2018-03-20 14:14:27 -0400

    factored out function for base64-encoding binary data

--- ramutils/utils.py
+++ ramutils/utils.py
@@ -9,6 +9,7 @@ import numpy as np
 import h5py
 import tempfile
 import shutil
+import base64
 
 from ramutils.log import get_logger
 from ptsa.data.readers import JsonIndexReader
@@ -377,3 +378,13 @@ def show_log_handlers():
     yield
     print("after")
     print_handlers()
+
+
+def encode_file(fd):
+    """
+    Produces a base64-encoded version of the data inside the file-like object fd.
+    :param fd:
+    :return: str
+    """
+    fd.seek(0)
+    return base64.b64encode(fd.read().decode())
