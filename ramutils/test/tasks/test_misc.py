78ba644a28a8240f4ddaf79b471a137dd1f22aa5
Zach Duey <zachduey@gmail.com>
2018-03-21 08:52:09 -0400

    Fixed outdated test

--- ramutils/test/tasks/test_misc.py
+++ ramutils/test/tasks/test_misc.py
@@ -15,18 +15,19 @@ datafile = functools.partial(resource_filename,
 @pytest.mark.rhino
 def test_build_report_from_cached_results():
 
-    target_selection_table, classifier_summaries, session_summaries, \
-    math_summaries, hmm_results = load_existing_results(
+    prior_results = load_existing_results(
         'R1354E', 'FR1', [1], False, datafile('input/report_db'),
         datafile('')).compute()
 
-    assert session_summaries is not None
-    assert classifier_summaries is not None
-    assert target_selection_table is not None
+    assert prior_results['session_summaries'] is not None
+    assert prior_results['classifier_evaluation_results'] is not None
+    assert prior_results['target_selection_table'] is not None
 
-    report = build_static_report('R1354E', 'FR1', session_summaries,
-                                 math_summaries, target_selection_table,
-                                 classifier_summaries,
+    report = build_static_report('R1354E', 'FR1',
+                                 prior_results['session_summaries'],
+                                 prior_results['math_summaries'],
+                                 prior_results['target_selection_table'],
+                                 prior_results['classifier_evaluation_results'],
                                  datafile('output/')).compute()
 
     assert report is not None
@@ -38,13 +39,11 @@ def test_build_report_from_cached_results():
         os.remove(f)
 
     # Check that non-existent data returns all None values
-    target_selection_table, classifier_summaries, session_summaries, \
-    math_summaries, hmm_results = load_existing_results(
+    prior_results = load_existing_results(
         'R1345D', 'FR1', [1], False, datafile('input/report_db'),
         datafile('')).compute()
 
-    results = [target_selection_table, classifier_summaries, session_summaries,
-               math_summaries, hmm_results]
+    results = [v for k,v in prior_results.items()]
 
     assert all([r is None for r in results])
     return
