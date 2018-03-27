0d452f99aa508c7739cfb0a6dc6f134768c42f29
Michael V. DePalatis <mike@depalatis.net>
2018-03-27 14:00:41 -0400

    Legacy Python fix

--- ramutils/__init__.py
+++ ramutils/__init__.py
@@ -7,5 +7,5 @@ version_info = namedtuple('VersionInfo', 'major,minor,patch')(*__version__.split
 
 # So we don't end up getting things logged multiple times
 _root_logger = logging.getLogger()
-_root_logger.handlers.clear()
+_root_logger.handlers = []
 _root_logger.addHandler(logging.NullHandler())
