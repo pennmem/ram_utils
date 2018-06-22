a354d2457268458c049dfe52e254cbc56d7f5566
Michael V. DePalatis <mike@depalatis.net>
2018-06-22 09:47:50 -0400

    Another pass at getting codecov to work

--- maint/run_tests_ci.sh
+++ maint/run_tests_ci.sh
@@ -4,4 +4,3 @@ mkdir test_directory
 cd test_directory
 PROJECT=$HOME/miniconda/envs/ramutils/lib/python3.6/site-packages/ramutils
 python -m ramutils.test -m "not rhino" --cov=$PROJECT -v
-cp .coverage ..
